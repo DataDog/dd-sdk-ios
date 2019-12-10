1129cf709c3ebaf58245f36b215f2cc581a0edee
Maciek Grzybowski <maciek.grzybowski@datadoghq.com>
2019-12-10 12:07:39 +0100

    RUMM-101 Add custom Xcode templates

---
+++ CONTRIBUTING.md
@@ -0,0 +1,14 @@
+# Contributing
+
+_TBD_
+
+## Coding Conventions
+
+We use custom Xcode file templates for creating `.swift` files and unit test classes. Install them with:
+
+```bash
+cd tools/xcode-templates
+./install-xcode-templates.sh 
+```
+
+💡 Note: restart of Xcode might be required.
---
+++ tools/xcode-templates/install-xcode-templates.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+XCODE_LOCATION=$(xcode-select -p)
+XCODE_TEMPLATES_LOCATION="$XCODE_LOCATION/Library/Xcode/Templates/File Templates/"
+
+if [ -z "$XCODE_LOCATION" ]; then
+	echo "🔥 Failed to install Xcode templates - cannot determine Xcode installation with \`xcode-select -p\`."
+	exit 1
+fi
+
+rm -r "$XCODE_TEMPLATES_LOCATION/Datadog" 2> /dev/null
+cp -R "./Datadog" "$XCODE_TEMPLATES_LOCATION"
+
+echo "✅ Installed Xcode templates"
