171a2a205748b626a61a8cf88813723d3907c2cb
Maciek Grzybowski <maciek.grzybowski@datadoghq.com>
2019-12-16 10:44:28 +0100

    RUMM-110 Do simple rename

--- Sources/Datadog/Logs/Upload/LogsUploader.swift
+++ Sources/Datadog/Logs/Upload/LogsUploader.swift
@@ -4,15 +4,15 @@ import Foundation
 final class LogsUploader {
     
     private let httpClient: HTTPClient
-    private let requestBuilder: LogsUploadRequestEncoder
+    private let requestEncoder: LogsUploadRequestEncoder
     
     init(configuration: Datadog, httpClient: HTTPClient) {
         self.httpClient = httpClient
-        self.requestBuilder = LogsUploadRequestEncoder(uploadURL: configuration.logsUploadURL)
+        self.requestEncoder = LogsUploadRequestEncoder(uploadURL: configuration.logsUploadURL)
     }
     
     func upload(logs: [Log], completion: @escaping (LogsDeliveryStatus) -> Void) throws {
-        let request = try requestBuilder.encodeRequest(with: logs)
+        let request = try requestEncoder.encodeRequest(with: logs)
         httpClient.send(request: request) { (result) in
             switch result {
             case .success(let httpResponse):
