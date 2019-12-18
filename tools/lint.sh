4a9f36796afe2e8e2a8de60386b62561de976cb3
Maciek Grzybowski <maciek.grzybowski@datadoghq.com>
2019-12-18 22:34:55 +0100

    RUMM-111 Improve exit codes for `./tools/lint.sh`

--- tools/lint.sh
+++ tools/lint.sh
@@ -6,7 +6,13 @@ if [ "$CURRENT_DIR" != "dd-sdk-ios" ]; then
 	echo "\`lint.sh\` must be run in repository root folder: \`./tools/lint.sh\`"; exit 1
 fi
 
-[[ -z "${XCODE_VERSION_ACTUAL}" ]] && REPORTER='emoji' || REPORTER="xcode"
-
-swiftlint lint --config ./tools/lint/sources.swiftlint.yml --reporter "$REPORTER"
-swiftlint lint --config ./tools/lint/tests.swiftlint.yml --reporter "$REPORTER"
+if [[ -z "${XCODE_VERSION_ACTUAL}" ]]; then
+	# when run from command line
+	set -e # exit with error code if `swiftlint lint` fails
+	swiftlint lint --config ./tools/lint/sources.swiftlint.yml --reporter "emoji" --strict
+	swiftlint lint --config ./tools/lint/tests.swiftlint.yml --reporter "emoji" --strict
+else
+	# when run by Xcode in Build Phase
+	swiftlint lint --config ./tools/lint/sources.swiftlint.yml --reporter "xcode"
+	swiftlint lint --config ./tools/lint/tests.swiftlint.yml --reporter "xcode"
+fi
