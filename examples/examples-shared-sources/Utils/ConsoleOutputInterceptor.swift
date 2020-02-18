66901a9d968785ebb63061c2efc7bd2eba9bafa4
Maciek Grzybowski <maciek.grzybowski@datadoghq.com>
2020-02-18 17:46:21 +0100

    RUMM-205 Add license comment to all *.swift files

--- examples/examples-shared-sources/Utils/ConsoleOutputInterceptor.swift
+++ examples/examples-shared-sources/Utils/ConsoleOutputInterceptor.swift
@@ -1,3 +1,9 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2019-2020 Datadog, Inc.
+ */
+
 /// Below `@testable import` is only for SDK debug purposes, to easily override internal `consolePrint` function and display
 /// the output of the `Logger` in UI. Should be never used in client's application code.
 @testable import Datadog
