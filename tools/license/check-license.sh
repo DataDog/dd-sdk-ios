b70bfc709b7a1dea9194b7f6ce79585e26fd8325
Maciek Grzybowski <maciek.grzybowski@datadoghq.com>
2020-02-19 14:24:13 +0100

    RUMM-205 Ignore Pakage.swift file for license check

--- tools/license/check-license.sh
+++ tools/license/check-license.sh
@@ -4,7 +4,7 @@ if [ ! -f "Package.swift" ]; then
 	echo "\`check-license.sh\` must be run in repository root folder: \`./tools/license/check-license.sh\`"; exit 1
 fi
 
-find . -type f -name \*.swift -print0 | while IFS= read -r -d '' file; do
+find . -type f \( -name "*.swift" ! -iname "Package.swift" \) -print0 | while IFS= read -r -d '' file; do
   if ! grep -q "Apache License Version 2.0" "$file"
   then
   	echo "No license in $file"
