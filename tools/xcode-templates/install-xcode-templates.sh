6e38db9b70da10e08a2610b1097d0b1bd98b0220
Mert Buran <buranmert@gmail.com>
2020-03-18 12:58:01 +0100

    RUMM-305 Xcode location is removed

--- tools/xcode-templates/install-xcode-templates.sh
+++ tools/xcode-templates/install-xcode-templates.sh
@@ -5,15 +5,8 @@ if [ ! -f "Package.swift" ]; then
     exit 1
 fi
 
-XCODE_LOCATION=$(xcode-select -p)
-
 SOURCE_TEMPLATES_LOCATION="./tools/xcode-templates/Datadog/"
 TARGET_TEMPLATES_LOCATION="$HOME/Library/Developer/Xcode/Templates/File Templates/Datadog"
-
-if [ -z "$XCODE_LOCATION" ]; then
-	echo "🔥 Failed to install Xcode templates - cannot determine Xcode installation with \`xcode-select -p\`."
-	exit 1
-fi
 
 rm -r "$TARGET_TEMPLATES_LOCATION" 2> /dev/null
 mkdir -p "$TARGET_TEMPLATES_LOCATION"
