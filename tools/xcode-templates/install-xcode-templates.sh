4cc71d37f7a370d3d7498830b518a9297658b947
Mert Buran <buranmert@gmail.com>
2020-03-18 12:12:34 +0100

    RUMM-305 Fixed: install-xcode-templates.sh
    installation directory changed
    from:/Applications/Xcode/...
    to:$HOME/Library/Xcode/...
    this fixes permission issues and also
    templates stay even xcode is updated

--- tools/xcode-templates/install-xcode-templates.sh
+++ tools/xcode-templates/install-xcode-templates.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
 if [ ! -f "Package.swift" ]; then
-    echo "\`install-xcode-templates.sh\` must be run in repository root folder: \`./tools/xcode-templates/install-xcode-templates.sh\`"; exit 1
+    echo "\`install-xcode-templates.sh\` must be run in repository root folder: \`./tools/xcode-templates/install-xcode-templates.sh\`"
+    exit 1
 fi
 
 XCODE_LOCATION=$(xcode-select -p)
-XCODE_TEMPLATES_LOCATION="$XCODE_LOCATION/Library/Xcode/Templates/File Templates/"
+
+SOURCE_TEMPLATES_LOCATION="./tools/xcode-templates/Datadog/"
+TARGET_TEMPLATES_LOCATION="$HOME/Library/Developer/Xcode/Templates/File Templates/Datadog"
 
 if [ -z "$XCODE_LOCATION" ]; then
 	echo "🔥 Failed to install Xcode templates - cannot determine Xcode installation with \`xcode-select -p\`."
 	exit 1
 fi
 
-rm -r "$XCODE_TEMPLATES_LOCATION/Datadog" 2> /dev/null
-cp -R "./tools/xcode-templates/Datadog" "$XCODE_TEMPLATES_LOCATION"
+rm -r "$TARGET_TEMPLATES_LOCATION" 2> /dev/null
+mkdir -p "$TARGET_TEMPLATES_LOCATION"
+cp -R "$SOURCE_TEMPLATES_LOCATION" "$TARGET_TEMPLATES_LOCATION"
+
+echo "✅ Datadog templates copied to: $TARGET_TEMPLATES_LOCATION"
+
+exit 0
