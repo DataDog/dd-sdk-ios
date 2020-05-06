30a315b8051da8c8a599238cadc8c5413aa94b63
Mert Buran <mert.buran@datadoghq.com>
2020-05-06 13:46:29 +0200

    RUMM-300 Rewrite of URLSession instrumentation code
    Now we use "isa swizzling"

--- Sources/Datadog/Core/HookedURLSession.swift
+++ Sources/Datadog/Core/HookedURLSession.swift
@@ -5,276 +5,91 @@
  */
 
 import Foundation
+import ObjectiveC.runtime
 
-public extension URLSession {
-    internal typealias RequestInterceptor = HookedSession.RequestInterceptor
-    internal typealias TaskObserver = HookedSession.TaskObserver
+// TODO: RUMM-300 Find a way to inject blocks/custom code to hooked methods
 
-    static func tracedSession(
-        configuration: URLSessionConfiguration = .default,
-        delegate: URLSessionDelegate? = nil,
-        delegateQueue: OperationQueue? = nil
-    ) -> URLSession {
-        let session = URLSession(
-            configuration: configuration,
-            delegate: delegate,
-            delegateQueue: delegateQueue
-        )
+private final class HookedURLSession: URLSession {
+    static let dummy = HookedURLSession()
 
-        let requestInterceptor: RequestInterceptor = {
-            // TODO: RUMM-300 Inject tracing headers
-            return $0
-        }
-
-        let taskObserver: TaskObserver = { _, _ in
-            // TODO: RUMM-300 Start/stop span based on state
-        }
-
-        let hookedSession = HookedSession(
-            session: session,
-            requestInterceptor: requestInterceptor,
-            taskObserver: taskObserver
-        )
-        return hookedSession.asURLSession()
-    }
-}
-
-/*
- HookedSession is a NSObject subclass.
- It keeps an URLSession instance inside.
- It relays all the method calls to URLSession,
- except those that are implemented by HookedSession.
-
- It intercepts and observes requests and tasks respectively.
- */
-internal final class HookedSession: NSObject {
-    typealias RequestInterceptor = (URLRequest) -> URLRequest
-    typealias TaskObserver = (URLSessionTask, URLSessionTask.State?) -> Void
-
-    private let session: URLSession
-    let requestInterceptor: RequestInterceptor
-    let taskObserver: TaskObserver
-    private var observations = [Int: NSKeyValueObservation](minimumCapacity: 100)
-
-    init(session: URLSession,
-         requestInterceptor: @escaping RequestInterceptor,
-         taskObserver: @escaping TaskObserver) {
-        self.session = session
-        self.requestInterceptor = requestInterceptor
-        self.taskObserver = taskObserver
-        super.init()
-    }
-
-    func asURLSession() -> URLSession {
-        let castedSession: URLSession = unsafeBitCast(self, to: URLSession.self)
-        return castedSession
-    }
-
-    // MARK: - Transparent messaging
-    /*
-     As a NSObject subclass yet exposed as URLSession (ref: URLSession.tracedSession)
-     all the method calls are `unrecognized_selector` for HookedSession, except
-     those which are implemented in HookedSession.
-     forwardingTarget passes those unimplemented method calls to session
-     */
-    override func forwardingTarget(for aSelector: Selector!) -> Any? { // swiftlint:disable:this implicitly_unwrapped_optional
-        return session
-    }
-
-    // MARK: - Helpers
-
-    /*
-     IMPORTANT NOTE:
-     If you create an URLSessionTask instance from an URLSession instance,
-     the task stays alive EVEN IF you nullify the session instance.
-     This happens because URLSessionTask has private `__taskGroup` property which keeps
-     session instance alive as long as the task is alive.
-
-     This is not the case for HookedSession instances.
-
-     let task = hookedSession.dataTask(...)
-     hookedSession = nil
-     task.resume()
-
-     In the case above, task and
-     hookedSession.session (private property) will stay alive
-     yet hookedSession will be deallocated.
-     That means observations will be deallocated too.
-     In order to keep observing until the task completes,
-     observationBlock below captures `self` on purpose.
-     Therefore, observationBlock keeps `self` alive
-     until the block is removed from self.observations dict.
-     */
-    private func observed<T: URLSessionTask>(_ task: T) -> T {
-        let observer = taskObserver
-        var previousState: URLSessionTask.State? = nil
-        let observation = task.observe(\.state, options: [.initial]) { observed, _ in
-            observer(observed, previousState)
-            previousState = observed.state
-
-            switch observed.state {
-            case .canceling, .completed:
-                self.observations[observed.taskIdentifier] = nil
-            default:
-                break
-            }
-        }
-        observations[task.taskIdentifier] = observation
-
-        return task
-    }
-
-    // MARK: - URLSessionDataTask
-
-    /*
-     IMPORTANT NOTE:
-     @objc enables dynamic method dispatch and
-     make sure @objc names match NSURLSession Obj-C interface.
-     Otherwise, these methods are not called.
-     */
     @objc(dataTaskWithURL:)
-    func dataTask(with url: URL) -> URLSessionDataTask {
+    override func dataTask(with url: URL) -> URLSessionDataTask {
+        print("BOOM")
         return dataTask(with: URLRequest(url: url))
     }
 
-    @objc(dataTaskWithURL:completionHandler:)
-    func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
-        return dataTask(with: URLRequest(url: url), completionHandler: completionHandler)
-    }
-
-    @objc(dataTaskWithRequest:)
-    func dataTask(with request: URLRequest) -> URLSessionDataTask {
-        return observed(session.dataTask(with: requestInterceptor(request)))
-    }
-
-    @objc(dataTaskWithRequest:completionHandler:)
-    func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
-        let task = session.dataTask(with: requestInterceptor(request),
-                                    completionHandler: completionHandler)
-        return observed(task)
-    }
-
-    // MARK: - URLSessionUploadTask
-
-    @objc(uploadTaskWithRequest:fromFile:)
-    func uploadTask(with request: URLRequest, fromFile fileURL: URL) -> URLSessionUploadTask {
-        return observed(session.uploadTask(with: requestInterceptor(request), fromFile: fileURL))
-    }
-
-    @objc(uploadTaskWithRequest:fromBodyData:)
-    func uploadTask(with request: URLRequest, from bodyData: Data) -> URLSessionUploadTask {
-        return observed(session.uploadTask(with: requestInterceptor(request), from: bodyData))
-    }
-
-    @objc(uploadTaskWithStreamedRequest:)
-    func uploadTask(withStreamedRequest request: URLRequest) -> URLSessionUploadTask {
-        return observed(session.uploadTask(withStreamedRequest: requestInterceptor(request)))
-    }
-
-    @objc(uploadTaskwithRequest:fromFile:completionHandler:)
-    func uploadTask(with request: URLRequest, fromFile fileURL: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionUploadTask {
-        let task = session.uploadTask(
-            with: requestInterceptor(request),
-            fromFile: fileURL,
-            completionHandler: completionHandler
-        )
-        return observed(task)
-    }
-
-    @objc(uploadTaskwithRequest:fromData:completionHandler:)
-    func uploadTask(with request: URLRequest, from bodyData: Data?, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionUploadTask {
-        let task = session.uploadTask(
-            with: requestInterceptor(request),
-            from: bodyData,
-            completionHandler: completionHandler
-        )
-        return observed(task)
-    }
-
-    // MARK: - URLSessionDownloadTask
-
-    @objc(downloadTaskWithURL:)
-    func downloadTask(with url: URL) -> URLSessionDownloadTask {
-        return downloadTask(with: URLRequest(url: url))
-    }
-
-    @objc(downloadTaskWithURL:completionHandler:)
-    func downloadTask(with url: URL, completionHandler: @escaping (URL?, URLResponse?, Error?) -> Void) -> URLSessionDownloadTask {
-        return downloadTask(with: URLRequest(url: url), completionHandler: completionHandler)
-    }
-
-    @objc(downloadTaskWithRequest:)
-    func downloadTask(with request: URLRequest) -> URLSessionDownloadTask {
-        return observed(session.downloadTask(with: requestInterceptor(request)))
-    }
-
-    @objc(downloadTaskWithRequest:completionHandler:)
-    func downloadTask(with request: URLRequest, completionHandler: @escaping (URL?, URLResponse?, Error?) -> Void) -> URLSessionDownloadTask {
-        let task = session.downloadTask(
-            with: requestInterceptor(request),
-            completionHandler: completionHandler
-        )
-        return observed(task)
-    }
-
-    @objc(downloadTaskWithResumeData:)
-    func downloadTask(withResumeData resumeData: Data) -> URLSessionDownloadTask {
-        return observed(session.downloadTask(withResumeData: resumeData))
-    }
-
-    @objc(downloadTaskWithResumeData:completionHandler:)
-    func downloadTask(withResumeData resumeData: Data, completionHandler: @escaping (URL?, URLResponse?, Error?) -> Void) -> URLSessionDownloadTask {
-        let task = session.downloadTask(
-            withResumeData: resumeData,
-            completionHandler: completionHandler
-        )
-        return observed(task)
-    }
+    func swiftFunctionNotToBeAdded() { }
 }
 
-// MARK: - Unsupported task types
+private final class HookedURLSessionTask: URLSessionTask {
+    static let dummy = HookedURLSessionTask()
+}
 
-// TODO: RUMM-300 are Stream/WebSocket supported by APM?
+internal final class Swizzler {
+    enum ObjCRuntimeError: Error {
+        case classCouldNotAllocated
+    }
 
-//extension HookedSession {
-//    // MARK: - URLSessionStreamTask
-//    @objc(streamTaskWithHostName:port:)
-//    func streamTask(withHostName hostname: String, port: Int) -> URLSessionStreamTask { }
-//    @objc(streamTaskWithService:)
-//    func streamTask(with service: NetService) -> URLSessionStreamTask { }
-//}
-//
-//@available(iOS 13, *)
-//extension HookedSession {
-//    // MARK: - URLSessionWebSocketTask
-//    @objc(webSocketTaskWithURL:)
-//    func webSocketTask(with url: URL) -> URLSessionWebSocketTask { }
-//    @objc(webSocketTaskWithURL:protocols:)
-//    func webSocketTask(with url: URL, protocols: [String]) -> URLSessionWebSocketTask { }
-//    @objc(webSocketTaskWithRequest:)
-//    func webSocketTask(with request: URLRequest) -> URLSessionWebSocketTask { }
-//}
+    static func isaSwizzle(_ object: AnyObject) throws {
+        let dynamicClass: AnyClass
+        if object.isKind(of: URLSession.self) {
+            dynamicClass = try dynamicSubclass(for: object, source: HookedURLSession.dummy)
+        } else if object.isKind(of: URLSessionTask.self) {
+            dynamicClass = try dynamicSubclass(for: object, source: HookedURLSessionTask.dummy)
+        } else {
+            // TODO: RUMM-300 Log this
+            return
+        }
+        object_setClass(object, dynamicClass)
+    }
 
-extension HookedSession: CustomReflectable {
-    /*
-     HookedSession imitates URLSession from outside in case that anyone does type-check
-     such as isKindOf:/isMemberOf: or checks superclass at runtime
-     */
-    var customMirror: Mirror {
-        return Mirror(reflecting: session)
+    // MARK: - Private
+
+    private static func dynamicSubclass(for object: AnyObject, source: AnyObject) throws -> AnyClass {
+        let dynamicClassPrefix = "_Datadog"
+        let superclass: AnyClass? = object_getClass(object)
+        let superclassName = class_getName(superclass)
+        // TODO: RUMM-300 Test %s with right-to-left languages
+        let dynamicClassName = String(format: "\(dynamicClassPrefix)%s", superclassName)
+        if let existingClass = objc_lookUpClass(dynamicClassName) {
+            return existingClass
+        }
+
+        guard let newClass = objc_allocateClassPair(superclass, dynamicClassName, 0),
+            let sourceClass = object_getClass(source) else {
+                throw ObjCRuntimeError.classCouldNotAllocated
+        }
+        addInstanceMethods(of: sourceClass, to: newClass)
+        objc_registerClassPair(newClass)
+        return newClass
     }
-    override func isKind(of aClass: AnyClass) -> Bool {
-        return session.isKind(of: aClass)
+
+    private static func addInstanceMethods(of sourceClass: AnyClass, to targetClass: AnyClass) {
+        let sourceMethodsCountPtr = UnsafeMutablePointer<UInt32>.allocate(capacity: 1)
+        defer { sourceMethodsCountPtr.deallocate() }
+
+        let copyMethodList: UnsafeMutablePointer<Method>? = class_copyMethodList(sourceClass,
+                                                                                 sourceMethodsCountPtr)
+        typealias Stride = UnsafeMutablePointer<Method>.Stride
+        let sourceMethodsCount = Stride(clamping: sourceMethodsCountPtr.pointee)
+        guard let sourceMethodsList = copyMethodList, sourceMethodsCount > 0 else {
+                return // No instance methods to add
+        }
+
+        for index in 0..<sourceMethodsCount {
+            let method: Method = sourceMethodsList.advanced(by: index).pointee
+            add(method: method, to: targetClass)
+        }
     }
-    override class func isKind(of aClass: AnyClass) -> Bool {
-        return URLSession.isKind(of: aClass)
+
+    private static func add(method: Method, to klass: AnyClass) {
+        let selector = method_getName(method)
+        let imp = method_getImplementation(method)
+        let types = method_getTypeEncoding(method)
+        let success = class_addMethod(klass, selector, imp, types)
+        if !success {
+            // TODO: RUMM-300 Log this case
+            return
+        }
     }
-    override func isMember(of aClass: AnyClass) -> Bool {
-        return session.isMember(of: aClass)
-    }
-    override class func isMember(of aClass: AnyClass) -> Bool {
-        return URLSession.isMember(of: aClass)
-    }
-    override var superclass: AnyClass? { return session.superclass }
-    override class func superclass() -> AnyClass? { return URLSession.superclass() }
 }
--- Tests/DatadogTests/Datadog/Core/HookedSessionTests.swift
+++ Tests/DatadogTests/Datadog/Core/HookedSessionTests.swift
@@ -5,107 +5,34 @@
  */
 
 import XCTest
+import ObjectiveC.runtime
 @testable import Datadog
-import _Datadog_Private
-
-private typealias RequestInterceptor = HookedSession.RequestInterceptor
-private typealias TaskObserver = HookedSession.TaskObserver
-
-private func buildSession(
-    with configuration: URLSessionConfiguration = URLSessionConfiguration.default,
-    interceptor: RequestInterceptor? = nil,
-    observer: TaskObserver? = nil,
-    sessionDelegate: URLSessionDelegate? = nil,
-    delegateQueue: OperationQueue? = nil
-) -> URLSession {
-    let session = URLSession(configuration: configuration,
-                             delegate: sessionDelegate,
-                             delegateQueue: delegateQueue)
-    let requestInterceptor = interceptor ?? { $0 }
-    let taskObserver = observer ?? { _, _ in }
-    return HookedSession(
-        session: session,
-        requestInterceptor: requestInterceptor,
-        taskObserver: taskObserver
-    ).asURLSession()
-}
 
 class HookedSessionTests: XCTestCase {
+    func testDebug() {
+        let session = URLSession.shared
+        let originalClass = String(cString: object_getClassName(session))
+
+        try? Swizzler.isaSwizzle(session)
+
+        let newClass = String(cString: object_getClassName(session))
+
+        XCTAssertNotEqual(originalClass, newClass)
+
+        _ = session.dataTask(with: URL(string: "https://foo.bar")!)
+        print("Done...")
+    }
+
     func testIntegrity() {
         final class TestSessionDelegate: NSObject, URLSessionDelegate { }
 
         let config = URLSessionConfiguration.background(withIdentifier: "unitTestIdentifier")
         let delegate = TestSessionDelegate()
-        let session = buildSession(with: config, sessionDelegate: delegate)
+        let session = URLSession(configuration: config, delegate: delegate, delegateQueue: nil)
 
         XCTAssertEqual(session.configuration.identifier, config.identifier)
         XCTAssertTrue(session.superclass is URLSession.Type)
         XCTAssertNotNil(session.delegate)
         XCTAssertEqual(session.delegate!.debugDescription, delegate.debugDescription)
-    }
-
-    func testSimpleTask() {
-        let session = buildSession()
-        let task = session.dataTask(with: URL(string: "https://foo.bar/1")!)
-        XCTAssertNotNil(task)
-    }
-
-    func testSimpleTaskCompletion() {
-        let session = buildSession()
-        let expect = expectation(description: "task completion")
-        let task = session.dataTask(with: URL(string: "https://news.ycombinator.com")!) { _, _, _ in
-            expect.fulfill()
-        }
-        task.resume()
-        wait(for: [expect], timeout: 3.0)
-    }
-
-    func testRequestInterception() {
-        let interceptionHeaders: [String: String] = ["interception": "success"]
-        let interceptor: RequestInterceptor = {
-            var newRequest = $0
-            newRequest.allHTTPHeaderFields = interceptionHeaders
-            return newRequest
-        }
-        let session = buildSession(with: .default, interceptor: interceptor)
-        let task = session.dataTask(with: URL(string: "https://foo.bar")!)
-
-        XCTAssertEqual(task.originalRequest?.allHTTPHeaderFields, interceptionHeaders)
-    }
-
-    func testTaskObservation() {
-        let runningExpc = expectation(description: "running expectation")
-        let completedExpc = expectation(description: "completed expectation")
-        let taskObserver: TaskObserver = { observed, previousState in
-            if case .running = observed.state, case .suspended = previousState {
-                runningExpc.fulfill()
-            }
-            if case .completed = observed.state, case .running = previousState {
-                completedExpc.fulfill()
-            }
-        }
-        let interceptor: RequestInterceptor = { $0 }
-        let session = buildSession(with: .default, interceptor: interceptor, observer: taskObserver)
-        let task = session.dataTask(with: URL(string: "https://foo.bar")!)
-        task.resume()
-
-        wait(for: [runningExpc, completedExpc], timeout: 1.0, enforceOrder: true)
-    }
-
-    func testUnrecognizedSelector() {
-        let exceptionHandler = ObjcExceptionHandler()
-        let session = buildSession()
-        var caughtException: NSError? = nil
-        do {
-            try exceptionHandler.rethrowToSwift {
-                session.perform(NSSelectorFromString("foo"))
-            }
-        } catch {
-            caughtException = error as NSError
-        }
-        XCTAssertNotNil(caughtException)
-        let exceptionReason = caughtException?.userInfo["reason"] as? String
-        XCTAssertNotNil(exceptionReason)
-        XCTAssertFalse(exceptionReason!.contains("HookedSession"))
     }
 }
