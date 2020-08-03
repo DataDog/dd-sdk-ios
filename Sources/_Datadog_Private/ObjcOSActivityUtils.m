a15d520a047b4ebfb6da45c74df990ffd2e4e32f
Nacho Bonafonte <nacho@undefinedlabs.com>
2020-08-03 16:58:24 +0200

    RUMM-612 Missed a license header in a file

--- Sources/_Datadog_Private/ObjcOSActivityUtils.m
+++ Sources/_Datadog_Private/ObjcOSActivityUtils.m
@@ -1,10 +1,8 @@
-//
-//  ObjcOSActivityUtils.m
-//  Datadog
-//
-//  Created by Ignacio Bonafonte Arruga on 03/08/2020.
-//  Copyright © 2020 Datadog. All rights reserved.
-//
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2019-2020 Datadog, Inc.
+ */
 
 #import <Foundation/Foundation.h>
 #import "ObjcOSActivityUtils.h"
