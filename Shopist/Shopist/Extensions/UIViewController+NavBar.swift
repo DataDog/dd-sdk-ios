bafc573a330a7ec671f50d7ee330c12748e9bf32
Mert Buran <mert.buran@datadoghq.com>
2020-08-05 12:34:11 +0200

    RUMM-602 Shopist: Cart added

---
+++ Shopist/Shopist/Extensions/UIViewController+NavBar.swift
@@ -0,0 +1,22 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2019-2020 Datadog, Inc.
+ */
+
+import UIKit
+
+extension UIViewController {
+    func addGoToCartButton() {
+        let cartButton = UIBarButtonItem(image: UIImage(systemName: "cart"), style: .plain, target: self, action: #selector(goToCart))
+        var buttonItems = navigationItem.rightBarButtonItems ?? []
+        buttonItems.append(cartButton)
+        navigationItem.rightBarButtonItems = buttonItems
+    }
+
+    @objc private func goToCart() {
+        let cartVC = CartViewController()
+        let containerVC = UINavigationController(rootViewController: cartVC)
+        present(containerVC, animated: true, completion: nil)
+    }
+}
