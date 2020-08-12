0119a656d300d75203221ffaaf70bafcba7e925a
Maciek Grzybowski <maciek.grzybowski@datadoghq.com>
2020-08-12 17:46:08 +0200

    RUMM-535 Create `DataUploadWorkerMock`

--- Sources/Datadog/Core/Persistence/FileWriter.swift
+++ Sources/Datadog/Core/Persistence/FileWriter.swift
@@ -19,7 +19,7 @@ internal final class FileWriter: FileWriterType {
     /// JSON encoder used to encode data.
     private let jsonEncoder: JSONEncoder
     /// Queue used to synchronize files access (read / write) and perform decoding on background thread.
-    private let queue: DispatchQueue
+    internal let queue: DispatchQueue
 
     init(dataFormat: DataFormat, orchestrator: FilesOrchestrator, queue: DispatchQueue) {
         self.dataFormat = dataFormat
