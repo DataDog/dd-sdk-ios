970ce3a52d742a222f85abe8874cde574752ba4a
Maciek Grzybowski <maciek.grzybowski@datadoghq.com>
2020-12-14 12:07:56 +0100

    RUMM-914 Structure `rum-models-generator` package into modules

--- tools/rum-models-generator/Package.swift
+++ tools/rum-models-generator/Package.swift
@@ -5,22 +5,26 @@ import PackageDescription
 
 let package = Package(
     name: "rum-models-generator",
+    platforms: [.macOS(.v10_15)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "rum-models-generator",
             targets: ["rum-models-generator"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.2.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "rum-models-generator",
-            dependencies: []),
+            dependencies: [
+                "RUMModelsGeneratorCore",
+                .product(name: "ArgumentParser", package: "swift-argument-parser")
+            ]),
+        .target(
+            name: "RUMModelsGeneratorCore",
+            dependencies: []
+        ),
         .testTarget(
             name: "rum-models-generatorTests",
             dependencies: ["rum-models-generator"]),
