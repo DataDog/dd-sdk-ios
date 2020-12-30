2cff3e124cc5f69675984c8f60587e5a636196b7
Maciek Grzybowski <maciek.grzybowski@datadoghq.com>
2020-12-30 11:46:58 +0100

    RUMM-917 Expose RUM APIs in `DDConfigurationBuilder`

--- Tests/DatadogTests/DatadogObjc/DDConfigurationTests.swift
+++ Tests/DatadogTests/DatadogObjc/DDConfigurationTests.swift
@@ -30,83 +30,91 @@ extension Datadog.Configuration.TracesEndpoint: Equatable {
 
 /// This tests verify that objc-compatible `DatadogObjc` wrapper properly interacts with`Datadog` public API (swift).
 class DDConfigurationTests: XCTestCase {
-    func testItFowardsInitializationToSwift() throws {
+    func testDefaultBuilderFowardsInitializationToSwift() throws {
         let objcBuilder = DDConfiguration.builder(clientToken: "abc-123", environment: "tests")
+        let objcRUMBuilder = DDConfiguration.builder(rumApplicationID: "rum-app-id", clientToken: "abc-123", environment: "tests")
 
-        let swiftConfigurationDefault = objcBuilder.build().sdkConfiguration
-        XCTAssertEqual(swiftConfigurationDefault.clientToken, "abc-123")
-        XCTAssertTrue(swiftConfigurationDefault.loggingEnabled)
-        XCTAssertTrue(swiftConfigurationDefault.tracingEnabled)
-        XCTAssertEqual(swiftConfigurationDefault.logsEndpoint, .us)
-        XCTAssertEqual(swiftConfigurationDefault.tracesEndpoint, .us)
-        XCTAssertEqual(swiftConfigurationDefault.environment, "tests")
-        XCTAssertNil(swiftConfigurationDefault.serviceName)
-        XCTAssertFalse(swiftConfigurationDefault.rumUIKitActionsTrackingEnabled)
-        XCTAssertNil(swiftConfigurationDefault.rumUIKitViewsPredicate)
+        let swiftConfiguration = objcBuilder.build().sdkConfiguration
+        let swiftConfigurationRUM = objcRUMBuilder.build().sdkConfiguration
+
+        XCTAssertFalse(swiftConfiguration.rumEnabled)
+        XCTAssertTrue(swiftConfigurationRUM.rumEnabled)
+
+        XCTAssertNil(swiftConfiguration.rumApplicationID)
+        XCTAssertEqual(swiftConfigurationRUM.rumApplicationID, "rum-app-id")
+
+        [swiftConfiguration, swiftConfigurationRUM].forEach { configuration in
+            XCTAssertEqual(configuration.clientToken, "abc-123")
+            XCTAssertTrue(configuration.loggingEnabled)
+            XCTAssertTrue(configuration.tracingEnabled)
+            XCTAssertEqual(configuration.logsEndpoint, .us)
+            XCTAssertEqual(configuration.tracesEndpoint, .us)
+            XCTAssertEqual(configuration.rumEndpoint, .us)
+            XCTAssertEqual(configuration.environment, "tests")
+            XCTAssertNil(configuration.serviceName)
+            XCTAssertNil(configuration.firstPartyHosts)
+            XCTAssertEqual(configuration.rumSessionsSamplingRate, 100.0)
+            XCTAssertNil(configuration.rumUIKitViewsPredicate)
+            XCTAssertFalse(configuration.rumUIKitActionsTrackingEnabled)
+        }
+    }
+
+    func testCustomizedBuilderFowardsInitializationToSwift() throws {
+        let objcBuilder = [
+            DDConfiguration.builder(clientToken: "abc-123", environment: "tests"),
+            DDConfiguration.builder(rumApplicationID: "rum-app-id", clientToken: "abc-123", environment: "tests")
+        ].randomElement()!
 
         objcBuilder.enableLogging(false)
-        let swiftConfigurationLoggingDisabled = objcBuilder.build().sdkConfiguration
-        XCTAssertFalse(swiftConfigurationLoggingDisabled.loggingEnabled)
+        XCTAssertFalse(objcBuilder.build().sdkConfiguration.loggingEnabled)
 
         objcBuilder.enableTracing(false)
-        let swiftConfigurationTracingDisabled = objcBuilder.build().sdkConfiguration
-        XCTAssertFalse(swiftConfigurationTracingDisabled.tracingEnabled)
+        XCTAssertFalse(objcBuilder.build().sdkConfiguration.tracingEnabled)
+
+        objcBuilder.enableRUM(false)
+        XCTAssertFalse(objcBuilder.build().sdkConfiguration.rumEnabled)
 
         objcBuilder.set(endpoint: .eu())
-        let swiftConfigurationEU = objcBuilder.build().sdkConfiguration
-        XCTAssertEqual(swiftConfigurationEU.datadogEndpoint, .eu)
+        XCTAssertEqual(objcBuilder.build().sdkConfiguration.datadogEndpoint, .eu)
 
         objcBuilder.set(endpoint: .us())
-        let swiftConfigurationUS = objcBuilder.build().sdkConfiguration
-        XCTAssertEqual(swiftConfigurationUS.datadogEndpoint, .us)
+        XCTAssertEqual(objcBuilder.build().sdkConfiguration.datadogEndpoint, .us)
 
         objcBuilder.set(endpoint: .gov())
-        let swiftConfigurationGov = objcBuilder.build().sdkConfiguration
-        XCTAssertEqual(swiftConfigurationGov.datadogEndpoint, .gov)
+        XCTAssertEqual(objcBuilder.build().sdkConfiguration.datadogEndpoint, .gov)
 
         let customLogsEndpoint = URL(string: "https://api.example.com/v1/logs")!
-        let customTracesEndpoint = URL(string: "https://api.example.com/v1/traces")!
         objcBuilder.set(customLogsEndpoint: customLogsEndpoint)
+        XCTAssertEqual(objcBuilder.build().sdkConfiguration.customLogsEndpoint, customLogsEndpoint)
+
+        let customTracesEndpoint = URL(string: "https://api.example.com/v1/traces")!
         objcBuilder.set(customTracesEndpoint: customTracesEndpoint)
-        let swiftConfigurationCustom = objcBuilder.build().sdkConfiguration
-        XCTAssertEqual(swiftConfigurationCustom.customLogsEndpoint, customLogsEndpoint)
-        XCTAssertEqual(swiftConfigurationCustom.customTracesEndpoint, customTracesEndpoint)
+        XCTAssertEqual(objcBuilder.build().sdkConfiguration.customTracesEndpoint, customTracesEndpoint)
+
+        let customRUMEndpoint = URL(string: "https://api.example.com/v1/rum")!
+        objcBuilder.set(customRUMEndpoint: customRUMEndpoint)
+        XCTAssertEqual(objcBuilder.build().sdkConfiguration.customRUMEndpoint, customRUMEndpoint)
 
         objcBuilder.set(serviceName: "service-name")
-        let swiftConfigurationServiceName = objcBuilder.build().sdkConfiguration
-        XCTAssertEqual(swiftConfigurationServiceName.serviceName, "service-name")
+        XCTAssertEqual(objcBuilder.build().sdkConfiguration.serviceName, "service-name")
 
         objcBuilder.track(firstPartyHosts: ["example.com"])
-        let swiftConfigurationTracedHosts = objcBuilder.build().sdkConfiguration
-        XCTAssertEqual(swiftConfigurationTracedHosts.firstPartyHosts, ["example.com"])
+        XCTAssertEqual(objcBuilder.build().sdkConfiguration.firstPartyHosts, ["example.com"])
 
         objcBuilder.trackUIKitActions()
-        let swiftConfigurationTrackingActions = objcBuilder.build().sdkConfiguration
-        XCTAssertTrue(swiftConfigurationTrackingActions.rumUIKitActionsTrackingEnabled)
-    }
-
-    func testDefaultViewPredicate() throws {
-        let objcBuilder = DDConfiguration.builder(clientToken: "abc-123", environment: "tests")
+        XCTAssertTrue(objcBuilder.build().sdkConfiguration.rumUIKitActionsTrackingEnabled)
 
         objcBuilder.trackUIKitRUMViews()
-        let swiftConfigurationViewPredicate = objcBuilder.build().sdkConfiguration
-        XCTAssertNotNil(swiftConfigurationViewPredicate.rumUIKitViewsPredicate)
-    }
+        XCTAssertTrue(objcBuilder.build().sdkConfiguration.rumUIKitViewsPredicate is DefaultUIKitRUMViewsPredicate)
 
-    func testCustomViewPredicate() throws {
         class ObjCPredicate: DDUIKitRUMViewsPredicate {
-            var returnView: DDRUMView?
-            func rumView(for viewController: UIViewController) -> DDRUMView? {
-                return returnView
-            }
+            func rumView(for viewController: UIViewController) -> DDRUMView? { nil }
         }
-
-        let objcBuilder = DDConfiguration.builder(clientToken: "abc-123", environment: "tests")
-
         let predicate = ObjCPredicate()
         objcBuilder.trackUIKitRUMViews(using: predicate)
-        let swiftConfigurationViewPredicate = objcBuilder.build().sdkConfiguration
-        let predicateBridge = try XCTUnwrap(swiftConfigurationViewPredicate.rumUIKitViewsPredicate as? PredicateBridge)
-        XCTAssert(predicateBridge.objcPredicate === predicate)
+        XCTAssertTrue((objcBuilder.build().sdkConfiguration.rumUIKitViewsPredicate as? PredicateBridge)?.objcPredicate === predicate)
+
+        objcBuilder.set(rumSessionsSamplingRate: 42.5)
+        XCTAssertEqual(objcBuilder.build().sdkConfiguration.rumSessionsSamplingRate, 42.5)
     }
 }
