fc6853a2be6efb0841e9598d5aa6e1c3227f0fbe
Alexandre Costanza <alexandre.costanza@datadoghq.com>
2021-01-18 21:07:15 +0100

    RUMM-786 Add asserts to check values of view.isActive in existing RUM Integration tests

--- Tests/DatadogIntegrationTests/Scenarios/RUM/RUMManualInstrumentationScenarioTests.swift
+++ Tests/DatadogIntegrationTests/Scenarios/RUM/RUMManualInstrumentationScenarioTests.swift
@@ -76,6 +76,8 @@ class RUMManualInstrumentationScenarioTests: IntegrationTests, RUMCommonAsserts 
         XCTAssertEqual(view1.errorEvents[0].error.resource?.url, "https://foo.com/resource/2")
         XCTAssertEqual(view1.errorEvents[0].error.resource?.method, .get)
         XCTAssertEqual(view1.errorEvents[0].error.resource?.statusCode, 400)
+        RUMSessionMatcher.assertViewWasInitiallyActive(view1)
+        RUMSessionMatcher.assertViewWasEventuallyInactive(view1)
 
         let contentReadyTiming = try XCTUnwrap(view1.viewEventMatchers.last?.timing(named: "content-ready"))
         let firstInteractionTiming = try XCTUnwrap(view1.viewEventMatchers.last?.timing(named: "first-interaction"))
@@ -91,11 +93,15 @@ class RUMManualInstrumentationScenarioTests: IntegrationTests, RUMCommonAsserts 
         XCTAssertEqual(view2.viewEvents.last?.view.error.count, 1)
         XCTAssertEqual(view2.errorEvents[0].error.message, "Simulated view error")
         XCTAssertEqual(view2.errorEvents[0].error.source, .source)
+        RUMSessionMatcher.assertViewWasInitiallyActive(view2)
+        RUMSessionMatcher.assertViewWasEventuallyInactive(view2)
 
         let view3 = session.viewVisits[2]
         XCTAssertEqual(view3.path, "SendRUMFixture3ViewController")
         XCTAssertEqual(view3.viewEvents.last?.view.action.count, 0)
         XCTAssertEqual(view3.viewEvents.last?.view.resource.count, 0)
         XCTAssertEqual(view3.viewEvents.last?.view.error.count, 0)
+        RUMSessionMatcher.assertViewWasInitiallyActive(view3)
+        RUMSessionMatcher.assertViewWasEventuallyInactive(view3)
     }
 }
