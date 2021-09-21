16f0a3e3b9625358cc8429edbbd1ebfad3bf2b3e
maxep <me@maxep.me>
2021-09-21 15:26:18 +0200

    RUMM-1609 Invoke Kronos from a single thread

--- Sources/Datadog/Core/System/Time/ServerDateProvider.swift
+++ Sources/Datadog/Core/System/Time/ServerDateProvider.swift
@@ -11,20 +11,28 @@ import Kronos
 internal protocol ServerDateProvider {
     /// Start the clock synchronisation with NTP server.
     /// Calls the `completion` by passing it the server time when the synchronization succeeds or`nil` if it fails.
-    func synchronize(with ntpPool: String, completion: @escaping (Date?) -> Void)
+    func synchronize(with pool: String, completion: @escaping (Date?) -> Void)
     /// Returns the server time or `nil` if not yet determined.
     /// This time gets more precise while synchronization is pending.
     func currentDate() -> Date?
 }
 
 internal class NTPServerDateProvider: ServerDateProvider {
-    func synchronize(with ntpPool: String, completion: @escaping (Date?) -> Void) {
-        Clock.sync(from: ntpPool, completion: { serverTime, _ in
-            completion(serverTime)
-        })
+    /// Queue used for invoking Kronos clock.
+    private let queue = DispatchQueue(
+        label: "com.datadoghq.date-provider",
+        target: .global(qos: .userInteractive)
+    )
+
+    func synchronize(with pool: String, completion: @escaping (Date?) -> Void) {
+        queue.async {
+            Clock.sync(from: pool, completion: { serverTime, _ in
+                completion(serverTime)
+            })
+        }
     }
 
     func currentDate() -> Date? {
-        return Clock.now
+        queue.sync { Clock.now }
     }
 }
