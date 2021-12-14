5f03f4e34e77872e970c18a3ac5b5ab6e4a55f84
Maciek Grzybowski <maciek.grzybowski@datadoghq.com>
2021-12-14 17:44:20 +0100

    RUMM-1765 Implement separate `Core` component for sampling
    so it can be used both in RUM and Crash Reporting for sampling
    session and/or crash report.

---
+++ Sources/Datadog/Core/Utils/Sampler.swift
@@ -0,0 +1,23 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2019-2020 Datadog, Inc.
+ */
+
+import Foundation
+
+/// Sampler, deciding if events should be sent do Datadog or dropped.
+internal struct Sampler {
+    /// Value between `0.0` and `100.0`, where `0.0` means NO event will be sent and `100.0` means ALL events will be sent.
+    let samplingRate: Float
+
+    init(samplingRate: Float) {
+        self.samplingRate = max(0, min(100, samplingRate))
+    }
+
+    /// Based on the sampling rate, it returns random value deciding if an event should be "sampled" or not.
+    /// - Returns: `true` if event should be sent to Datadog and `false` if it should be dropped.
+    func sample() -> Bool {
+        return Float.random(in: 0.0..<100.0) < samplingRate
+    }
+}
