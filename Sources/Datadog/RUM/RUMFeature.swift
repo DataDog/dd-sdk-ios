fa7bb87f413c2b0d291deec0a138390c01dcee54
Maxime Epain <maxime.epain@datadoghq.com>
2022-04-27 17:45:46 +0200

    RUMM-2027 Enable cpu vital telemetry

--- Sources/Datadog/RUM/RUMFeature.swift
+++ Sources/Datadog/RUM/RUMFeature.swift
@@ -147,7 +147,7 @@ internal final class RUMFeature {
             upload: upload,
             configuration: configuration,
             commonDependencies: commonDependencies,
-            vitalCPUReader: VitalCPUReader(),
+            vitalCPUReader: VitalCPUReader(telemetry: telemetry),
             vitalMemoryReader: VitalMemoryReader(),
             vitalRefreshRateReader: VitalRefreshRateReader(),
             onSessionStart: configuration.onSessionStart
