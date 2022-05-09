6d989925df5c167b46b506b9fe85be04fd91cc68
Maxime Epain <maxime.epain@datadoghq.com>
2022-05-09 10:40:54 +0200

    RUMM-2134 Prevent data size overflows

--- Sources/Datadog/Core/Persistence/DataBlock.swift
+++ Sources/Datadog/Core/Persistence/DataBlock.swift
@@ -14,10 +14,11 @@ internal enum BlockType: UInt16 {
     case event = 0x00
 }
 
-/// Reported errors while manipulateing data blocks.
+/// Reported errors while manipulating data blocks.
 internal enum DataBlockError: Error {
     case readOperationFailed(streamError: Error?)
     case invalidByteSequence
+    case dataLenghtExceedsLimit
 }
 
 /// A data block in defined by its type and a byte sequence.
@@ -39,12 +40,15 @@ internal struct DataBlock {
     ///     +------------+---------------+-----------+
     ///
     /// - Returns: a data block in TLV.
-    func serialize() -> Data {
+    func serialize() throws -> Data {
         var buffer = Data()
         // T
         withUnsafeBytes(of: type.rawValue) { buffer.append(contentsOf: $0) }
         // L
-        withUnsafeBytes(of: BlockSize(data.count)) { buffer.append(contentsOf: $0) }
+        guard let length = BlockSize(exactly: data.count) else {
+            throw DataBlockError.dataLenghtExceedsLimit
+        }
+        withUnsafeBytes(of: length) { buffer.append(contentsOf: $0) }
         // V
         buffer += data
         return buffer
@@ -61,12 +65,22 @@ internal final class DataBlockReader {
 
     /// Reads block from data input.
     ///
-    /// At initilization, the reader will open a stream targeting the input
-    /// data. The stream will be closed when the reader instance is deallocated.
+    /// At initilization, the reader will open a stream targeting the input data.
+    /// The stream will be closed when the reader instance is deallocated.
     ///
     /// - Parameter data: The data input
-    init(data: Data) {
-        stream = InputStream(data: data)
+    convenience init(data: Data) {
+        self.init(input: InputStream(data: data))
+    }
+
+    /// Reads block from an input stream.
+    ///
+    /// At initilization, the reader will open the stream, it will be closed
+    /// when the reader instance is deallocated.
+    ///
+    /// - Parameter stream: The input stream
+    init(input stream: InputStream) {
+        self.stream = stream
         stream.open()
     }
 
@@ -112,20 +126,20 @@ internal final class DataBlockReader {
         return blocks
     }
 
-    /// Reads `lenght` bytes from stream.
+    /// Reads `length` bytes from stream.
     ///
-    /// - Parameter lenght: The number of byte to read
+    /// - Parameter length: The number of byte to read
     /// - Throws: `DataBlockError` while reading the input stream.
     /// - Returns: Data bytes from stream.
-    private func read(lenght: Int) throws -> Data {
-        var bytes = [UInt8](repeating: 0, count: lenght)
-        let count = stream.read(&bytes, maxLength: lenght)
+    private func read(length: Int) throws -> Data {
+        var bytes = [UInt8](repeating: 0, count: length)
+        let count = stream.read(&bytes, maxLength: length)
 
         if count < 0 {
             throw DataBlockError.readOperationFailed(streamError: stream.streamError)
         }
 
-        guard count == lenght else {
+        guard count == length else {
             throw DataBlockError.invalidByteSequence
         }
 
@@ -134,14 +148,22 @@ internal final class DataBlockReader {
 
     /// Reads a block type.
     private func readType() throws -> BlockType.RawValue {
-        let data = try read(lenght: MemoryLayout<BlockType.RawValue>.size)
+        let data = try read(length: MemoryLayout<BlockType.RawValue>.size)
         return data.withUnsafeBytes { $0.load(as: BlockType.RawValue.self) }
     }
 
-    /// Reads  block data.
+    /// Reads block data.
     private func readData() throws -> Data {
-        let data = try read(lenght: MemoryLayout<BlockSize>.size)
+        let data = try read(length: MemoryLayout<BlockSize>.size)
         let size = data.withUnsafeBytes { $0.load(as: BlockSize.self) }
-        return try read(lenght: Int(size))
+
+        // even if `Int` is able to represent all `BlockSize` on 64 bit
+        // arch, we make sure to avoid overflow and get the exact data
+        // length.
+        guard let length = Int(exactly: size) else {
+            throw DataBlockError.dataLenghtExceedsLimit
+        }
+
+        return try read(length: length)
     }
 }
--- Tests/DatadogTests/Datadog/Core/Persistence/DataBlockTests.swift
+++ Tests/DatadogTests/Datadog/Core/Persistence/DataBlockTests.swift
@@ -10,9 +10,27 @@ import XCTest
 class DataBlockTests: XCTestCase {
     func testSerializeDataBlock() throws {
         XCTAssertEqual(
-            DataBlock(type: .event, data: Data([0xFF])).serialize(),
+            try DataBlock(type: .event, data: Data([0xFF])).serialize(),
             Data([0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0xFF])
         )
+    }
+
+    func testSerialize_zeroBytesBlock() throws {
+        XCTAssertEqual(
+            try DataBlock(type: .event, data: Data()).serialize(),
+            Data([0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
+        )
+    }
+
+    func testSerialize_largeBytesBlock() throws {
+        let data = try DataBlock(
+            type: .event,
+            data: .mockRepeating(byte: 0xFF, times: 10_000_000) // 10MB
+        ).serialize()
+
+        XCTAssertEqual(data.count, 10_000_006)
+        // TLV representation: T=0x0000, L=0x00989680, V[0]=0xFF
+        XCTAssertEqual(data.prefix(7), Data([0x00, 0x00, 0x80, 0x96, 0x98, 0x00, 0xFF]))
     }
 
     func testDataBlockReader_withSingleBlock() throws {
@@ -24,8 +42,8 @@ class DataBlockTests: XCTestCase {
     }
 
     func testDataBlockReader_withMultipleBlock() throws {
-        let data = (0..<100).map { size in
-            DataBlock(
+        let data = try (0..<100).map { size in
+            try DataBlock(
                 type: .event,
                 data: .mock(ofSize: size)
             ).serialize()
@@ -38,5 +56,53 @@ class DataBlockTests: XCTestCase {
         XCTAssertEqual(blocks.count, 100)
         XCTAssertEqual(blocks.first?.data.count, 0)
         XCTAssertEqual(blocks.last?.data.count, 99)
+    }
+
+    func testDataBlockReader_skipUnknownType() throws {
+        let data = Data(
+            [
+                0x00, 0xFF, 0x01, 0x00, 0x00, 0x00, 0xFF,
+                0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0xFF,
+                0x00, 0xFF, 0x01, 0x00, 0x00, 0x00, 0xFF
+            ]
+        )
+        let reader = DataBlockReader(data: data)
+        let block = try reader.next()
+        XCTAssertEqual(block?.type, .event)
+        XCTAssertEqual(block?.data, Data([0xFF]))
+        XCTAssertNil(try reader.next())
+    }
+
+    func testDataBlockReader_readsZeroBytesBlock() throws {
+        let data = Data(
+            [
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0xFF,
+                0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0xFF, 0xFF
+            ]
+        )
+        let reader = DataBlockReader(data: data)
+
+        var block = try reader.next()
+        XCTAssertEqual(block?.type, .event)
+        XCTAssertEqual(block?.data, Data())
+
+        block = try reader.next()
+        XCTAssertEqual(block?.type, .event)
+        XCTAssertEqual(block?.data, Data([0xFF]))
+
+        block = try reader.next()
+        XCTAssertEqual(block?.type, .event)
+        XCTAssertEqual(block?.data, Data([0xFF, 0xFF]))
+    }
+
+    func testDataBlockReader_readsLargeBytesBlock() throws {
+        let data = Data([0x00, 0x00, 0x80, 0x96, 0x98, 0x00]) + Data.mockRepeating(byte: 0xFF, times: 10_000_000) // 10MB
+        let reader = DataBlockReader(data: data)
+
+        let block = try reader.next()
+        XCTAssertEqual(block?.type, .event)
+        XCTAssertEqual(block?.data.first, 0xFF)
+        XCTAssertEqual(block?.data.count, 10_000_000)
     }
 }
