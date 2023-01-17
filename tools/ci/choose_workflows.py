eee637bc868f1322b5b3a7e1ca132c30453d594c
Maciej Burda <maciej.burda@datadoghq.com>
2023-01-17 10:30:14 +0000

    RUMM-2690 PR fixes

--- tools/ci/choose_workflows.py
+++ tools/ci/choose_workflows.py
@@ -27,6 +27,7 @@ def should_run_unit_tests(ctx: CIContext) -> bool:
             'Tests/DatadogTests',
             'Tests/DatadogCrashReportingTests',
             'Datadog/Datadog.xcodeproj/',
+            'TestUtilities/',
         ],
         pr_file_extensions=[]
     )
