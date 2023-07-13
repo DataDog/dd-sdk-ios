9cca06cc53b2a8bbb715e7aee7c338f33969c198
Maciek Grzybowski <maciek.grzybowski@datadoghq.com>
2023-07-13 17:07:32 +0200

    REPLAY-1892 Sanitize invalid CGColor values before passing them to Recorder

---
+++ DatadogSessionReplay/Sources/Recorder/Utilities/CFType+Safety.swift
@@ -0,0 +1,41 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2019-Present Datadog, Inc.
+ */
+
+import CoreGraphics
+
+/// Sanitizes and validates Core Foundation types.
+///
+/// Verifies if the given `value` is of the expected type specified by `expectedTypeID`.
+/// Returns the value if the type matches; otherwise, returns `nil`.
+///
+/// This method is used to sanitize attributes that can be set dynamically, leveraging the Objective-C runtime. For example:
+///
+/// ```
+/// // If an invalid value is set dynamically (e.g., with User Defined Runtime Attributes in Storyboard):
+/// view.setValue("string value", forKeyPath: "layer.borderColor")
+///
+/// // The following will crash:
+/// let alpha = view.layer.borderColor?.alpha
+///
+/// // Sanitizing it will return `nil` and prevent the crash:
+/// let alpha = sanitize(value: view.layer.borderColor, expectedTypeID: CGColor.typeID)?.alpha
+/// ```
+///
+/// For full context, see: https://github.com/DataDog/dd-sdk-ios/pull/1373
+///
+/// Reference: [CFTypeRef - Core Foundation](https://developer.apple.com/documentation/corefoundation/cftyperef)
+private func sanitize<T: CFTypeRef>(value: T?, expectedTypeID: CFTypeID) -> T? {
+    guard let value = value, CFGetTypeID(value) == expectedTypeID else {
+        return nil
+    }
+    return value
+}
+
+internal extension CGColor {
+    /// Casts receiver to valid `CGColor` object.
+    /// Returns value only if this underlying `CFTypeRef` is of `CGColor.typeID` type.
+    var safeCast: CGColor? { sanitize(value: self, expectedTypeID: CGColor.typeID) }
+}
---
+++ DatadogSessionReplay/Tests/Utilities/CFType+SafetyTests.swift
@@ -0,0 +1,20 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2019-Present Datadog, Inc.
+ */
+
+import Foundation
+import XCTest
+@testable import DatadogSessionReplay
+
+class CFTypeSafetyTests: XCTestCase {
+    func testInvalidCGColorValueIsSanitized() {
+        let valid: CGColor = UIColor.red.cgColor
+        XCTAssertEqual(valid, valid.safeCast)
+
+        let string: Any = "invalid CGColor value"
+        let invalid = string as! CGColor
+        XCTAssertNil(invalid.safeCast)
+    }
+}
