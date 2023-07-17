fd99da39024a4df11b1c175d6f14bac681b78a23
Maxime Epain <maxime.epain@datadoghq.com>
2023-07-17 17:41:29 +0200

    Fix benchmark tests

--- BenchmarkTests/DataStorage/LoggingStorageBenchmarkTests.swift
+++ BenchmarkTests/DataStorage/LoggingStorageBenchmarkTests.swift
@@ -100,6 +100,11 @@ class LoggingStorageBenchmarkTests: XCTestCase {
             threadName: "main",
             applicationVersion: "0.0.0",
             dd: .init(device: .init(architecture: "testArch")),
+            os: .init(
+                name: "OS",
+                version: "1.0.0",
+                build: "FFFFF"
+            ),
             userInfo: .init(id: "abc-123", name: "foo", email: "foo@bar.com", extraInfo: ["str": "value", "int": 11_235, "bool": true]),
             networkConnectionInfo: .init(
                 reachability: .yes,
