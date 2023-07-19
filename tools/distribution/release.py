47573b74644eb1535e1491ea8b6771e3cc9d2250
Maxime Epain <maxime.epain@datadoghq.com>
2023-07-19 11:19:49 +0200

    Fix xcframework assets for Carthage

--- tools/distribution/release.py
+++ tools/distribution/release.py
@@ -45,12 +45,6 @@ if __name__ == "__main__":
         default=os.environ.get('DD_RELEASE_OVERWRITE_GITHUB') == '1'
     )
     parser.add_argument(
-        "--add-xcode-version-to-github-asset",
-        action='store_true',
-        help="Add Xcode version to the GH Release asset.",
-        default=os.environ.get('DD_ADD_XCODE_VERSION_TO_GITHUB_ASSET') == '1'
-    )
-    parser.add_argument(
         "--dry-run",
         action='store_true',
         help="Run validation but skip publishing.",
@@ -63,7 +57,6 @@ if __name__ == "__main__":
         only_github = True if args.only_github else False
         only_cocoapods = True if args.only_cocoapods else False
         overwrite_github = True if args.overwrite_github else False
-        add_xcode_version_to_github_asset = True if args.add_xcode_version_to_github_asset else False
         dry_run = True if args.dry_run else False
 
         # Validate arguments:
@@ -72,9 +65,6 @@ if __name__ == "__main__":
 
         if only_cocoapods and overwrite_github:
             raise Exception('`--overwrite-github` and `--only-cocoapods` cannot be used together.')
-
-        if only_cocoapods and add_xcode_version_to_github_asset:
-            raise Exception('--add-xcode-version-to-github-asset` and `--only-cocoapods` cannot be used together.')
 
         version = Version(git_tag)
         if not version:
@@ -86,7 +76,6 @@ if __name__ == "__main__":
               f'- DD_RELEASE_ONLY_GITHUB                = {os.environ.get("DD_RELEASE_ONLY_GITHUB")}\n'
               f'- DD_RELEASE_ONLY_COCOAPODS             = {os.environ.get("DD_RELEASE_ONLY_COCOAPODS")}\n'
               f'- DD_RELEASE_OVERWRITE_GITHUB           = {os.environ.get("DD_RELEASE_OVERWRITE_GITHUB")}\n'
-              f'- DD_ADD_XCODE_VERSION_TO_GITHUB_ASSET  = {os.environ.get("DD_ADD_XCODE_VERSION_TO_GITHUB_ASSET")}\n'
               f'- DD_RELEASE_DRY_RUN                    = {os.environ.get("DD_RELEASE_DRY_RUN")}')
 
         print(f'🛠️️ ENV and CLI arguments resolved to:\n'
@@ -94,7 +83,6 @@ if __name__ == "__main__":
               f'- only_github                        = {only_github}\n'
               f'- only_cocoapods                     = {only_cocoapods}\n'
               f'- overwrite_github                   = {overwrite_github}\n'
-              f'- add_xcode_version_to_github_asset  = {add_xcode_version_to_github_asset}\n'
               f'- dry_run                            = {dry_run}.')
 
         print(f'🛠️ Git tag read to version: {version}')
@@ -119,7 +107,7 @@ if __name__ == "__main__":
 
             # Publish GH Release asset:
             if publish_to_gh:
-                gh_asset = GHAsset(add_xcode_version=add_xcode_version_to_github_asset,git_tag=git_tag)
+                gh_asset = GHAsset(git_tag=git_tag)
                 gh_asset.validate()
                 gh_asset.publish(overwrite_existing=overwrite_github, dry_run=dry_run)
 
