f1f4e7f5247bd18784f1d76312ee0aa01f5bb799
Maciek Grzybowski <maciek.grzybowski@datadoghq.com>
2023-08-01 19:32:10 +0200

    Add ENV configuration

---
+++ BenchmarkTests/BenchmarkTests/Environment.swift
@@ -0,0 +1,38 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2019-Present Datadog, Inc.
+ */
+
+import Foundation
+
+internal struct Environment {
+    struct InfoPlistKey {
+        static let clientToken      = "DatadogClientToken"
+        static let rumApplicationID = "RUMApplicationID"
+    }
+
+    static func readClientToken() -> String {
+        guard let clientToken = Bundle.main.infoDictionary?[InfoPlistKey.clientToken] as? String, !clientToken.isEmpty else {
+            fatalError("""
+            ✋⛔️ Cannot read `\(InfoPlistKey.clientToken)` from `Info.plist` dictionary.
+            Please update `Datadog.xcconfig` in the repository root with your own
+            client token obtained on datadoghq.com.
+            You might need to run `Product > Clean Build Folder` before retrying.
+            """)
+        }
+        return clientToken
+    }
+
+    static func readRUMApplicationID() -> String {
+        guard let rumApplicationID = Bundle.main.infoDictionary![InfoPlistKey.rumApplicationID] as? String, !rumApplicationID.isEmpty else {
+            fatalError("""
+            ✋⛔️ Cannot read `\(InfoPlistKey.rumApplicationID)` from `Info.plist` dictionary.
+            Please update `Datadog.xcconfig` in the repository root with your own
+            RUM application id obtained on datadoghq.com.
+            You might need to run `Product > Clean Build Folder` before retrying.
+            """)
+        }
+        return rumApplicationID
+    }
+}
