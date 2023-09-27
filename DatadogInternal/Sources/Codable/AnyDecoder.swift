a5c007988ab9f68d5235fe3673846f1606667c48
Maxime Epain <maxime.epain@datadoghq.com>
2023-09-27 17:09:34 +0200

    Remove PassthroughValueDecodingContainer

--- DatadogInternal/Sources/Codable/AnyDecoder.swift
+++ DatadogInternal/Sources/Codable/AnyDecoder.swift
@@ -598,7 +598,7 @@ private class _AnyDecoder: Decoder {
 
     /// A container that can support the storage and direct decoding of a single
     /// nonkeyed value.
-    struct SingleValueContainer: SingleValueDecodingContainer, PassthroughValueDecodingContainer {
+    struct SingleValueContainer: SingleValueDecodingContainer {
         /// The path of coding keys taken to get to this point in encoding.
         let codingPath: [CodingKey]
 
@@ -777,17 +777,6 @@ private class _AnyDecoder: Decoder {
             return try T(from: decoder)
         }
 
-        /// Decodes a Passthrough value.
-        ///
-        /// - returns: Whether the encountered value was passthrough.
-        func decodePassthrough() throws -> PassthroughAnyCodable {
-            guard let value = value as? PassthroughAnyCodable else {
-                throw DecodingError.typeMismatch(PassthroughAnyCodable.self, in: self)
-            }
-
-            return value
-        }
-
         /// Converts value to any `BinaryInteger`.
         ///
         /// - Parameter type: The `BinaryInteger` to convert as.
@@ -868,9 +857,25 @@ private extension DecodingError {
     }
 }
 
-internal protocol PassthroughValueDecodingContainer where Self: SingleValueDecodingContainer {
-    /// Decodes a Passthrough value.
+extension SingleValueDecodingContainer {
+    /// Decodes a passthrough value of the given type.
     ///
-    /// - returns: Whether the encountered value was passthrough.
-    func decodePassthrough() throws -> PassthroughAnyCodable
+    /// This method can succeed only when using `AnyDecoder`.
+    ///
+    /// - returns: A passthrough value if any.
+    /// - throws: `DecodingError.typeMismatch` if the value was not passthrough.
+    func passthrough() throws -> PassthroughAnyCodable {
+        guard let container = self as? _AnyDecoder.SingleValueContainer else {
+            throw DecodingError.typeMismatch(PassthroughAnyCodable.self, DecodingError.Context(
+                codingPath: codingPath,
+                debugDescription: "Single Value Container cannot decode passthrough value."
+            ))
+        }
+
+        guard let value = container.value as? PassthroughAnyCodable else {
+            throw DecodingError.typeMismatch(PassthroughAnyCodable.self, in: container)
+        }
+
+        return value
+    }
 }
