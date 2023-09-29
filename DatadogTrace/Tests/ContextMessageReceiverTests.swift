009c6902b60559a3aa4ff87ebe163a4f814663bb
Maxime Epain <maxime.epain@datadoghq.com>
2023-09-29 15:43:42 +0200

    Test context propagation in Trace

--- DatadogTrace/Tests/ContextMessageReceiverTests.swift
+++ DatadogTrace/Tests/ContextMessageReceiverTests.swift
@@ -6,6 +6,7 @@
 
 import XCTest
 import TestUtilities
+import DatadogInternal
 
 @testable import DatadogTrace
 
@@ -65,6 +66,56 @@ class ContextMessageReceiverTests: XCTestCase {
         XCTAssertEqual(receiver.context.rum?["_dd.session.id"], "session-id")
         XCTAssertNil(receiver.context.rum?["_dd.view.id"])
         XCTAssertNil(receiver.context.rum?["_dd.action.id"])
+    }
+
+    func testItReceivesNilRUMContext() throws {
+        // Given
+        let receiver = ContextMessageReceiver(bundleWithRumEnabled: true)
+        let core = PassthroughCoreMock(
+            context: .mockWith(
+                baggages: [
+                    "rum": .init([
+                        "application.id": "app-id",
+                        "session.id": "session-id",
+                        "view.id": "view-id",
+                        "user_action.id": "action-id"
+                    ])
+                ]
+            ),
+            messageReceiver: receiver
+        )
+
+        XCTAssertEqual(receiver.context.rum?["_dd.application.id"], "app-id")
+        XCTAssertEqual(receiver.context.rum?["_dd.session.id"], "session-id")
+        XCTAssertEqual(receiver.context.rum?["_dd.view.id"], "view-id")
+        XCTAssertEqual(receiver.context.rum?["_dd.action.id"], "action-id")
+
+        // When
+        core.set(baggage: nil, forKey: "rum")
+
+        // Then
+        XCTAssertNil(receiver.context.rum)
+    }
+
+    func testItReceivesMalformedRUMContext() throws {
+        // Given
+        let telemetryReceiver = TelemetryMock()
+        let contextReceiver = ContextMessageReceiver(bundleWithRumEnabled: true)
+        let core = PassthroughCoreMock(
+            messageReceiver: CombinedFeatureMessageReceiver([
+                contextReceiver,
+                telemetryReceiver
+            ])
+        )
+
+        // When
+        core.set(baggage: "malformed RUM context", forKey: "rum")
+
+        // Then
+        XCTAssertNil(contextReceiver.context.rum)
+
+        let error = try XCTUnwrap(telemetryReceiver.messages.first?.asError)
+        XCTAssert(error.message.contains("Fails to decode RUM context from Trace - typeMismatch"))
     }
 
     func testItIngnoresRUMContext() throws {
