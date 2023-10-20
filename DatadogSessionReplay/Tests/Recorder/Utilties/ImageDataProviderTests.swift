f1f66acb253986c8d169e5a843798c98cf2e95f4
Maciej Burda <maciej.burda@datadoghq.com>
2023-10-20 13:12:01 +0100

    RUM-1507 Add tests

--- DatadogSessionReplay/Tests/Recorder/Utilties/ImageDataProviderTests.swift
+++ DatadogSessionReplay/Tests/Recorder/Utilties/ImageDataProviderTests.swift
@@ -8,29 +8,38 @@ import XCTest
 @testable import DatadogSessionReplay
 
 class ImageDataProviderTests: XCTestCase {
-    func test_returnsEmptyString_WhenContentIsEmpty() {
+    func test_returnsNil_WhenImageIsNil() {
         let sut = ImageDataProvider()
-        let imageString = sut.contentBase64String(of: UIImage())
 
-        XCTAssertEqual(imageString, "")
+        XCTAssertNil(sut.contentBase64String(of: nil))
     }
 
-    func test_returnsValidString_WhenContentIsValid() throws {
+    func test_returnsEmptyStringAndIdentifier_WhenContentIsEmpty() {
+        let sut = ImageDataProvider()
+        let imageResource = sut.contentBase64String(of: UIImage())
+
+        XCTAssertEqual(imageResource?.base64, "")
+        XCTAssertEqual(imageResource?.identifier, "")
+    }
+
+    func test_returnsValidStringAndIdentifier_WhenContentIsValid() throws {
         let sut = ImageDataProvider()
         let base64 = "R0lGODlhAQABAIAAAP7//wAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=="
         let image = UIImage(data: Data(base64Encoded: base64)!)
 
-        let imageData = try XCTUnwrap(sut.contentBase64String(of: image))
-        XCTAssertGreaterThan(imageData.count, 0)
+        let imageResource = try XCTUnwrap(sut.contentBase64String(of: image))
+        XCTAssertGreaterThan(imageResource.base64.count, 0)
+        XCTAssertEqual(imageResource.identifier, "536081631ad95d6f784c21dc5a3b3609")
     }
 
     @available(iOS 13.0, *)
-    func test_returnsValidString_forSFSymbolIcon() throws {
+    func test_returnsValidStringAndIdentifier_forSFSymbolIcon() throws {
         let sut = ImageDataProvider()
         let image = UIImage(systemName: "square.and.arrow.up")
 
-        let imageData = try XCTUnwrap(sut.contentBase64String(of: image))
-        XCTAssertGreaterThan(imageData.count, 0)
+        let imageResource = try XCTUnwrap(sut.contentBase64String(of: image))
+        XCTAssertGreaterThan(imageResource.base64.count, 0)
+        XCTAssertEqual(imageResource.identifier, "ae0bb15c82ec2dcddf937a0948ca9720")
     }
 
     func test_imageIdentifierConsistency() {
