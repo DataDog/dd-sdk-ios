1f6d754eb368a2e84384d0410c398c490e519436
Maciek Grzybowski <maciek.grzybowski@datadoghq.com>
2024-01-23 13:12:14 +0100

    RUM-2099 Conditionally compile deprecated iOS 11 code only in Xcode 15
    this is to avoid compiler warnings raised from using APIs deprecated in iOS 12.

--- DatadogCore/Sources/Core/Context/CarrierInfoPublisher.swift
+++ DatadogCore/Sources/Core/Context/CarrierInfoPublisher.swift
@@ -64,6 +64,12 @@ extension CarrierInfo {
 
 // MARK: - iOS 11
 
+// Xcode 15 ships with Swift 5.9 and drops support for iOS 11 deployment target.
+// The conditional check ensures that iOS 11 components are only compiled with Xcode 14, where iOS 11
+// remains a valid deployment target. Since this deployment target is no longer available in Xcode 15,
+// compiling the following code in Xcode 15 would result in compiler warnings (due to deprecation in some used APIs),
+// violating our 0 warnings policy. It can be entirely removed when Xcode 14 is dropped.
+#if swift(<5.9)
 /// Carrier info provider for iOS 11.
 /// It reads `CarrierInfo?` from `CTTelephonyNetworkInfo` each time.
 internal struct iOS11CarrierInfoReader: ContextValueReader {
@@ -95,6 +101,7 @@ extension CarrierInfo {
         )
     }
 }
+#endif
 
 extension CarrierInfo.RadioAccessTechnology {
     init(_ radioAccessTechnology: String) {
