341ddb8055bd9707193b04bfa878cc37ff2fe2d8
Maciej Burda <maciej.burda@datadoghq.com>
2024-01-29 17:09:04 +0000

    RUM-2154 Update licence header

--- DatadogSessionReplay/Sources/Recorder/ViewTreeSnapshotProducer/ViewTreeSnapshot/NodeRecorders/UIImageResource.swift
+++ DatadogSessionReplay/Sources/Recorder/ViewTreeSnapshotProducer/ViewTreeSnapshot/NodeRecorders/UIImageResource.swift
@@ -1,10 +1,8 @@
-//
-//  UIImageResource.swift
-//  DatadogSessionReplay iOS
-//
-//  Created by Maciej Burda on 17/01/2024.
-//  Copyright © 2024 Datadog. All rights reserved.
-//
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2019-Present Datadog, Inc.
+ */
 
 #if os(iOS)
 import UIKit
