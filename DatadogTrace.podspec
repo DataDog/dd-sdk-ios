1f7521277f2843a98777cb4c51e176fe6c9056af
Ganesh Jangir <ganesh.jangir@datadoghq.com>
2024-02-14 14:47:25 +0100

    wip

--- DatadogTrace.podspec
+++ DatadogTrace.podspec
@@ -23,5 +23,4 @@ Pod::Spec.new do |s|
   s.source_files = ["DatadogTrace/Sources/**/*.swift"]
 
   s.dependency 'DatadogInternal', s.version.to_s
-  s.dependency 'OpenTelemetrySwiftApi', '1.9.1'
 end
--- tools/distribution/build-xcframework.sh
+++ tools/distribution/build-xcframework.sh
@@ -90,7 +90,6 @@ rm -rf $OUTPUT
 carthage bootstrap --platform $PLATFORM --use-xcframeworks
 mkdir -p "$XCFRAMEWORK_OUTPUT"
 cp -r "Carthage/Build/CrashReporter.xcframework" "$XCFRAMEWORK_OUTPUT"
-cp -r "Carthage/Build/OpenTelemetryApi.xcframework" "$XCFRAMEWORK_OUTPUT"
 
 bundle DatadogInternal
 bundle DatadogCore
