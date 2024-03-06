b0ae691fe52e61f174e321b3a29cf1007b505315
Ganesh Jangir <ganesh.jangir@datadoghq.com>
2024-03-06 10:58:15 +0100

    RUM-1836 feat(otel-tracer): test case to translate resource.name attribute to resource

--- DatadogTrace/Tests/OpenTelemetry/OTelSpanTests.swift
+++ DatadogTrace/Tests/OpenTelemetry/OTelSpanTests.swift
@@ -31,6 +31,27 @@ final class OTelSpanTests: XCTestCase {
         XCTAssertEqual(recordedSpan.operationName, "OperationName")
     }
 
+    func testSpanResourceNameAttribute() {
+        let writeSpanExpectation = expectation(description: "write span event")
+        let core = PassthroughCoreMock(expectation: writeSpanExpectation)
+
+        // Given
+        let tracer: DatadogTracer = .mockWith(core: core)
+        let span = tracer.spanBuilder(spanName: "OperationName").startSpan()
+
+        // When
+        span.setAttribute(key: "resource.name", value: "ResourceName")
+        span.end()
+
+        // Then
+        waitForExpectations(timeout: 0.5, handler: nil)
+        let recordedSpans = core.spans()
+        XCTAssertEqual(recordedSpans.count, 1)
+        let recordedSpan = recordedSpans.first!
+        XCTAssertEqual(recordedSpan.resource, "ResourceName")
+        XCTAssertEqual(recordedSpan.operationName, "OperationName")
+    }
+
     func testSpanSetName() {
         let writeSpanExpectation = expectation(description: "write span event")
         let core = PassthroughCoreMock(expectation: writeSpanExpectation)
