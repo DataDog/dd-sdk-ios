138678600d144e276bb0e7ada4543a39305d126e
Maciej Burda <maciej.burda@datadoghq.com>
2024-03-26 10:46:15 +0000

    RUM-3569 Use data store to remove resource duplicates between sessions

--- DatadogSessionReplay/Sources/Writers/ResourcesWriter.swift
+++ DatadogSessionReplay/Sources/Writers/ResourcesWriter.swift
@@ -16,25 +16,55 @@ internal protocol ResourcesWriting {
 
 internal class ResourcesWriter: ResourcesWriting {
     /// An instance of SDK core the SR feature is registered to.
-    private weak var core: DatadogCoreProtocol?
+    private let scope: FeatureScope?
+    private let telemetry: Telemetry
+
+    @ReadWriteLock
+    private var knownIdentifiers = Set<String>() {
+        didSet {
+            if let knownIdentifiers = try? JSONEncoder().encode(knownIdentifiers) {
+                scope?.dataStore.setValue(
+                    knownIdentifiers,
+                    forKey: Constants.processedResourcesKey
+                )
+            }
+        }
+    }
 
     init(
         core: DatadogCoreProtocol
     ) {
-        self.core = core
+        self.scope = core.scope(for: ResourcesFeature.name)
+        self.telemetry = core.telemetry
+
+        self.scope?.dataStore.value(forKey: Constants.processedResourcesKey) { [weak self] result in
+            switch result {
+            case .value(let data, _):
+                if let knownIdentifiers = try? JSONDecoder().decode(Set<String>.self, from: data) {
+                    self?.knownIdentifiers.formUnion(knownIdentifiers)
+                }
+            case .error(let error):
+                self?.telemetry.error("Failed to read processed resources from data store: \(error)")
+            case .noValue:
+                break
+            }
+        }
     }
 
     // MARK: - Writing
 
     func write(resources: [EnrichedResource]) {
-        guard let scope = core?.scope(for: ResourcesFeature.self) else {
-            return
-        }
-        scope.eventWriteContext { _, recordWriter in
-            resources.forEach {
-                recordWriter.write(value: $0)
+        scope?.eventWriteContext { [weak self] _, recordWriter in
+            let unknownResources = resources.filter { self?.knownIdentifiers.contains($0.identifier) == false }
+            for resource in unknownResources {
+                recordWriter.write(value: resource)
             }
+            self?.knownIdentifiers.formUnion(Set(unknownResources.map { $0.identifier }))
         }
+    }
+
+    private enum Constants {
+        static let processedResourcesKey = "processed-resources"
     }
 }
 #endif
