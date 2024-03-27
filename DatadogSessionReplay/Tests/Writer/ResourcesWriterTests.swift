342386b6441531bd21dc7e9f342287f46d9441dc
Maciej Burda <maciej.burda@datadoghq.com>
2024-03-27 10:41:06 +0000

    RUM-3569 Refactor

--- DatadogSessionReplay/Tests/Writer/ResourcesWriterTests.swift
+++ DatadogSessionReplay/Tests/Writer/ResourcesWriterTests.swift
@@ -40,27 +40,28 @@ class ResourcesWriterTests: XCTestCase {
         XCTAssertEqual(core.events(ofType: EnrichedResource.self).count, 0)
     }
 
-    func testWhenFeatureScopeIsConnected_itWritesResourcesToCore_andRemovesDuplicates() throws {
+    func testWritesSameResourcesToCore_andRemovesDuplicates() throws {
         // Given
         let dataStore = DataStoreMock()
+        dataStore.values[ResourcesWriter.Constants.storeCreationKey] = Date().timeIntervalSince1970.asData()
         let core = PassthroughCoreMock(dataStore: dataStore)
 
         // When
         let writer = ResourcesWriter(core: core)
+        writer.write(resources: [.mockWith(identifier: "1")])
+        writer.write(resources: [.mockWith(identifier: "1")])
 
         // Then
-        writer.write(resources: [.mockWith(identifier: "1")])
-        writer.write(resources: [.mockWith(identifier: "1")])
-
         XCTAssertEqual(core.events(ofType: EnrichedResource.self).count, 1)
-        let data = try XCTUnwrap(dataStore.values["processed-resources"])
+        let data = try XCTUnwrap(dataStore.values[ResourcesWriter.Constants.knownResourcesKey])
         XCTAssertGreaterThan(data.count, 0)
     }
 
-    func testWhenFeatureScopeIsConnected_itWritesResourcesToCore_andReadsKnownDuplicates() throws {
+    func testWhenReadsKnownDuplicates_itDoesNotWriteRecordsToCore() throws {
         // Given
         let dataStore = DataStoreMock()
-        dataStore.values["processed-resources"] = try JSONEncoder().encode(Set(["1"]))
+        dataStore.values[ResourcesWriter.Constants.knownResourcesKey] = Set(["1"]).asData()
+        dataStore.values[ResourcesWriter.Constants.storeCreationKey] = Date().timeIntervalSince1970.asData()
         let core = PassthroughCoreMock(dataStore: dataStore)
 
         // When
@@ -68,7 +69,36 @@ class ResourcesWriterTests: XCTestCase {
 
         // Then
         writer.write(resources: [.mockWith(identifier: "1")])
-
         XCTAssertEqual(core.events(ofType: EnrichedResource.self).count, 0)
+    }
+
+    func testWhenDataStoreIsOlderThan30Days_itClearsKnownDuplicates() throws {
+        // Given
+        let dataStore = DataStoreMock()
+        dataStore.values[ResourcesWriter.Constants.knownResourcesKey] = Set(["2", "1"]).asData()
+        dataStore.values[ResourcesWriter.Constants.storeCreationKey] = (Date().timeIntervalSince1970 - 31.days).asData()
+        let core = PassthroughCoreMock(dataStore: dataStore)
+
+        // When
+        let writer = ResourcesWriter(core: core)
+        XCTAssertNil(dataStore.values[ResourcesWriter.Constants.knownResourcesKey])
+
+        // Then
+        writer.write(resources: [.mockWith(identifier: "1")])
+        XCTAssertEqual(core.events(ofType: EnrichedResource.self).count, 1)
+        XCTAssertEqual(dataStore.values[ResourcesWriter.Constants.knownResourcesKey], Set(["1"]).asData())
+    }
+
+    func testWhenInitialized_itSetsUpDataStore() {
+        // Given
+        let dataStore = DataStoreMock()
+        let core = PassthroughCoreMock(dataStore: dataStore)
+
+        // When
+        _ = ResourcesWriter(core: core)
+
+        // Then
+        XCTAssertNotNil(dataStore.values[ResourcesWriter.Constants.storeCreationKey])
+        XCTAssertNil(dataStore.values[ResourcesWriter.Constants.knownResourcesKey])
     }
 }
