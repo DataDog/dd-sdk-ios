bc3610a7fa32ccfe21874a0bc62673c61e2c26ca
Maciek Grzybowski <maciek.grzybowski@datadoghq.com>
2024-05-10 11:03:34 +0200

    RUM-2911 CR feedback - test `CrashContext` coding against the actual encoder and decoder
    + simplify ISO8601 date coding

--- DatadogCore/Tests/Datadog/CrashReporting/CrashContext/CrashContextTests.swift
+++ DatadogCore/Tests/Datadog/CrashReporting/CrashContext/CrashContextTests.swift
@@ -12,8 +12,10 @@ import TestUtilities
 @testable import DatadogCrashReporting
 
 class CrashContextTests: XCTestCase {
-    private let encoder = JSONEncoder()
-    private let decoder = JSONDecoder()
+    /// This must be the exact encoder used to encode `CrashContext` in production code.
+    private let encoder = CrashReportingFeature.crashContextEncoder
+    /// This must be the exact decoder used to decode `CrashContext` in production code.
+    private let decoder = CrashReportingFeature.crashContextDecoder
 
     func testGivenContextWithTrackingConsentSet_whenItGetsEncoded_thenTheValueIsPreservedAfterDecoding() throws {
         let randomConsent: TrackingConsent = .mockRandom()
@@ -129,6 +131,7 @@ class CrashContextTests: XCTestCase {
 
     func testGivenContextWithAppLaunchDate_whenItGetsEncoded_thenTheValueIsPreservedAfterDecoding() throws {
         let randomDate: Date = .mockRandom()
+
         // Given
         let context: CrashContext = .mockWith(appLaunchDate: randomDate)
 
@@ -137,6 +140,10 @@ class CrashContextTests: XCTestCase {
 
         // Then
         let deserializedContext = try decoder.decode(CrashContext.self, from: serializedContext)
-        XCTAssertEqual(deserializedContext.appLaunchDate, randomDate)
+        XCTAssertEqual(
+            deserializedContext.appLaunchDate!.timeIntervalSince1970,
+            randomDate.timeIntervalSince1970,
+            accuracy: 0.001 // assert with ms precision as we encode dates as ISO8601 string which is lossfull
+        )
     }
 }
