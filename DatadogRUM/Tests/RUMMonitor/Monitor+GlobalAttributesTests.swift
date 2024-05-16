7d69e12e9a70b827e4c4ce9df392bdfcf8597982
Maciek Grzybowski <maciek.grzybowski@datadoghq.com>
2024-05-16 13:51:49 +0200

    RUM-3588 CR feedback - add tests for updating attributes

--- DatadogRUM/Tests/RUMMonitor/Monitor+GlobalAttributesTests.swift
+++ DatadogRUM/Tests/RUMMonitor/Monitor+GlobalAttributesTests.swift
@@ -116,6 +116,24 @@ class Monitor_GlobalAttributesTests: XCTestCase {
         XCTAssertEqual(lastView.attribute(forKey: "attribute2"), "value2")
     }
 
+    func testUpdatingGlobalAttributesAfterSDKInit_thenStartingAndStoppingView() throws {
+        // Given
+        monitor.notifySDKInit()
+
+        // When
+        monitor.addAttribute(forKey: "attribute", value: "value")
+        monitor.addAttribute(forKey: "attribute", value: "new-value")
+        monitor.removeAttribute(forKey: "unknown-attribute")
+        monitor.startView(key: "View")
+        monitor.stopView(key: "View")
+
+        // Then
+        let lastView = try XCTUnwrap(featureScope.eventsWritten(ofType: RUMViewEvent.self).last)
+        XCTAssertEqual(lastView.view.name, "View")
+        XCTAssertEqual(lastView.attribute(forKey: "attribute"), "new-value")
+        XCTAssertEqual(lastView.numberOfAttributes, 1)
+    }
+
     // MARK: - Changing Global Attributes After Starting View
 
     func testAddingGlobalAttributeAfterViewIsStarted() throws {
@@ -242,6 +260,39 @@ class Monitor_GlobalAttributesTests: XCTestCase {
         XCTAssertEqual(lastView2.attribute(forKey: "attribute3"), "value3")
 
         XCTAssertNil(lastView3.attribute(forKey: "attribute1"))
+        XCTAssertEqual(lastView3.attribute(forKey: "attribute2"), "value2")
+        XCTAssertEqual(lastView3.attribute(forKey: "attribute3"), "value3")
+    }
+
+    func testAddingGlobalAttributesAfterViewIsStarted_thenStartingNextViewsWhileUpdatingAttributes() throws {
+        // Given
+        monitor.notifySDKInit()
+        monitor.startView(key: "View1")
+        monitor.addAttribute(forKey: "attribute1", value: "value1")
+        monitor.addAttribute(forKey: "attribute2", value: "value2")
+        monitor.addAttribute(forKey: "attribute3", value: "value3")
+
+        // When
+        monitor.startView(key: "View2")
+        monitor.addAttribute(forKey: "attribute1", value: "new-value1")
+        monitor.startView(key: "View3")
+        monitor.addAttribute(forKey: "attribute2", value: "new-value1")
+
+        // Then
+        let viewEvents = featureScope.eventsWritten(ofType: RUMViewEvent.self)
+        let lastView1 = try XCTUnwrap(viewEvents.last(where: { $0.view.name == "View1" }))
+        let lastView2 = try XCTUnwrap(viewEvents.last(where: { $0.view.name == "View2" }))
+        let lastView3 = try XCTUnwrap(viewEvents.last(where: { $0.view.name == "View3" }))
+
+        XCTAssertEqual(lastView1.attribute(forKey: "attribute1"), "value1")
+        XCTAssertEqual(lastView1.attribute(forKey: "attribute2"), "value2")
+        XCTAssertEqual(lastView1.attribute(forKey: "attribute3"), "value3")
+
+        XCTAssertEqual(lastView2.attribute(forKey: "attribute1"), "new-value1")
+        XCTAssertEqual(lastView2.attribute(forKey: "attribute2"), "value2")
+        XCTAssertEqual(lastView2.attribute(forKey: "attribute3"), "value3")
+
+        XCTAssertEqual(lastView3.attribute(forKey: "attribute1"), "new-value1")
         XCTAssertEqual(lastView3.attribute(forKey: "attribute2"), "value2")
         XCTAssertEqual(lastView3.attribute(forKey: "attribute3"), "value3")
     }
@@ -382,6 +433,34 @@ class Monitor_GlobalAttributesTests: XCTestCase {
         XCTAssertEqual(resourceEvent.context?.contextInfo["attribute2"] as? String, "value2")
     }
 
+    func testUpdatingGlobalAttributeInActiveView_thenCollectingViewEvents() throws {
+        // Given
+        monitor.notifySDKInit()
+        monitor.startView(key: "ActiveView")
+        monitor.addAttribute(forKey: "attribute", value: "value")
+        monitor.addAttribute(forKey: "attribute", value: "new-value")
+
+        // When
+        monitor.addError(message: "error event")
+        monitor.addAction(type: .custom, name: "custom action event")
+        monitor.startResource(resourceKey: "resource", url: .mockAny())
+        monitor.stopResource(resourceKey: "resource", response: .mockAny())
+
+        // Then
+        let lastView = try XCTUnwrap(featureScope.eventsWritten(ofType: RUMViewEvent.self).last)
+        let errorEvent = try XCTUnwrap(featureScope.eventsWritten(ofType: RUMErrorEvent.self).last)
+        let actionEvent = try XCTUnwrap(featureScope.eventsWritten(ofType: RUMActionEvent.self).last)
+        let resourceEvent = try XCTUnwrap(featureScope.eventsWritten(ofType: RUMResourceEvent.self).last)
+
+        XCTAssertEqual(lastView.view.error.count, 1)
+        XCTAssertEqual(lastView.view.action.count, 1)
+        XCTAssertEqual(lastView.view.resource.count, 1)
+        XCTAssertNil(lastView.attribute(forKey: "attribute"))
+        XCTAssertEqual(errorEvent.context?.contextInfo["attribute"] as? String, "new-value")
+        XCTAssertEqual(actionEvent.context?.contextInfo["attribute"] as? String, "new-value")
+        XCTAssertEqual(resourceEvent.context?.contextInfo["attribute"] as? String, "new-value")
+    }
+
     func testAddingGlobalAttributesToActiveView_thenCollectingViewTimingsAndRemovingAttribute() throws {
         // Given
         monitor.notifySDKInit()
@@ -415,5 +494,9 @@ private extension RUMViewEvent {
 
     func attribute<T: Equatable>(forKey key: String) -> T? {
         return context?.contextInfo[key] as? T
+    }
+
+    var numberOfAttributes: Int {
+        return context?.contextInfo.count ?? 0
     }
 }
