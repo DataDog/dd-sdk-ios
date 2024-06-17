a01658979b62d6086d3c01543070f623a51f9be8
Ganesh Jangir <ganesh.jangir@datadoghq.com>
2024-06-17 17:52:48 +0200

    RUM-3463 feat(watchdog-termination): fix linter

--- DatadogInternal/Sources/Context/Sysctl.swift
+++ DatadogInternal/Sources/Context/Sysctl.swift
@@ -118,7 +118,7 @@ public struct Sysctl: SysctlProviding {
         var info = kinfo_proc()
         var mib: [Int32] = [CTL_KERN, KERN_PROC, KERN_PROC_PID, getpid()]
         var size = MemoryLayout<kinfo_proc>.stride
-        let junk = sysctl(&mib, UInt32(mib.count), &info, &size, nil, 0)
+        _ = sysctl(&mib, UInt32(mib.count), &info, &size, nil, 0)
         return (info.kp_proc.p_flag & P_TRACED) != 0
     }
 }
