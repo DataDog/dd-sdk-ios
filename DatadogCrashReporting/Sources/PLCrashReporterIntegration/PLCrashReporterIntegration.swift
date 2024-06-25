04449b87247a837d9862535927b792e7b405f350
Maciek Grzybowski <maciek.grzybowski@datadoghq.com>
2024-06-25 10:30:49 +0200

    Customize PLCR settings

--- DatadogCrashReporting/Sources/PLCrashReporterIntegration/PLCrashReporterIntegration.swift
+++ DatadogCrashReporting/Sources/PLCrashReporterIntegration/PLCrashReporterIntegration.swift
@@ -22,9 +22,9 @@ internal extension PLCrashReporterConfig {
         return PLCrashReporterConfig(
             // The choice of `.BSD` over `.mach` is well discussed here:
             // https://github.com/microsoft/PLCrashReporter/blob/7f27b272d5ff0d6650fc41317127bb2378ed6e88/Source/CrashReporter.h#L238-L363
-            signalHandlerType: .BSD,
+            signalHandlerType: .mach,
             // We don't symbolicate on device. All symbolication will happen backend-side.
-            symbolicationStrategy: [],
+            symbolicationStrategy: [.all],
             // Set a custom path to avoid conflicts with other PLC instances
             basePath: directory.path
         )
