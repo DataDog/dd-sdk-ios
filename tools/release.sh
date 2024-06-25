e3456a0c9e421ec1cbf81d759a0ac20198c888ed
Maciek Grzybowski <maciek.grzybowski@datadoghq.com>
2024-06-25 17:23:46 +0200

    WIP

--- tools/release.sh
+++ tools/release.sh
@@ -7,9 +7,9 @@ set -eo pipefail
 source ./tools/utils/argparse.sh
 source ./tools/utils/echo_color.sh
 
-set_description "Deploys release --artifact for given --tag."
-define_arg "tag" "" "The tag to deploy artifact for (must be a tag that exists in remote)." "string" "true"
-define_arg "artifact" "" "The type of artifact to deploy" "string" "true"
+# set_description "Deploys release --artifact for given --tag."
+# define_arg "tag" "" "The tag to deploy artifact for (must be a tag that exists in remote)." "string" "true"
+# define_arg "artifact" "" "The type of artifact to deploy" "string" "true"
 
 DISTRIBUTION_PACKAGE="tools/distribution"
 
@@ -17,4 +17,4 @@ echo_subtitle "Run 'make clean install' in '$DISTRIBUTION_PACKAGE'"
 cd $DISTRIBUTION_PACKAGE && make clean install
 cd -
 
-
+git clone --branch 2.12.0 --single-branch git@github.com:DataDog/dd-sdk-ios.git
