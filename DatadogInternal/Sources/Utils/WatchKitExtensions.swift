a9549d17e0d0d7231e5107f0c218133c38ff79e2
Jakub Fiser <141125531+jfiser-paylocity@users.noreply.github.com>
2024-06-27 15:05:39 +0200

    Replace personal file header with the prefefined one

--- DatadogInternal/Sources/Utils/WatchKitExtensions.swift
+++ DatadogInternal/Sources/Utils/WatchKitExtensions.swift
@@ -1,10 +1,8 @@
-//
-//  WatchKitExtensions.swift
-//  Datadog
-//
-//  Created by Jakub Fiser on 26.06.2024.
-//  Copyright © 2024 Datadog. All rights reserved.
-//
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2019-Present Datadog, Inc.
+ */
 
 import Foundation
 
