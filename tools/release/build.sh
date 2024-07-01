ed8fa6cbe149ee61add372ce122c9884caa7f73a
Maciek Grzybowski <maciek.grzybowski@datadoghq.com>
2024-07-01 19:59:47 +0200

    TEST2

--- tools/release/build.sh
+++ tools/release/build.sh
@@ -15,28 +15,30 @@ rm -rf "$artifacts_path"
 mkdir -p "$artifacts_path"
 
 REPO_CLONE_PATH="$artifacts_path/dd-sdk-ios"
-XCFRAMEWORKS_OUTPUT_PATH="$artifacts_path/xcframeworks"
-XCFRAMEWORKS_ZIP_NAME="Datadog.xcframework.zip"
-XCFRAMEWORKS_ZIP_PATH="$artifacts_path/$XCFRAMEWORKS_ZIP_NAME"
+XCF_DIR_NAME="Datadog.xcframework"
+XCF_ZIP_NAME="Datadog.xcframework.zip"
 
 # Clone a fresh version of the repo to artifacts path. This is to ensure that distribution tool
-# from current repo will operate on a clean version of the repo not altered by any configuration changes.
+# from current repo will operate on a clean version of the repo not altered by any configuration changes:
 echo_subtitle "Cloning repo for '$tag' into '$artifacts_path'"
 git clone --depth 1 --branch $tag --single-branch git@github.com:DataDog/dd-sdk-ios.git $REPO_CLONE_PATH
 
-# Build XCFrameworks using distribution tools from current repo.
-echo_subtitle "Creating XCFrameworks for '$tag' in '$XCFRAMEWORKS_OUTPUT_PATH'"
+# Build xcframeworks using distribution tools from current repo:
+echo_subtitle "Creating '$XCF_DIR_NAME' in '$artifacts_path'"
 ./tools/release/build-xcframeworks.sh --repo-path "$REPO_CLONE_PATH" \
     --ios --tvos \
-    --output-path $XCFRAMEWORKS_OUTPUT_PATH
+    --output-path "$artifacts_path/$XCF_DIR_NAME"
 
-# Zip XCFrameworks
-echo_subtitle "Creating '$XCFRAMEWORKS_ZIP_PATH' for '$tag'"
-DIR=$(pwd)
-cd $XCFRAMEWORKS_OUTPUT_PATH && zip -q --symlinks -r $XCFRAMEWORKS_ZIP_NAME *.xcframework
-cd $DIR
-mv -v "$XCFRAMEWORKS_OUTPUT_PATH/$XCFRAMEWORKS_ZIP_NAME" "$XCFRAMEWORKS_ZIP_PATH"
-rm -rf $XCFRAMEWORKS_OUTPUT_PATH
+echo_info "Contents of '$artifacts_path/$XCF_DIR_NAME':"
+ls $artifacts_path/$XCF_DIR_NAME
 
-echo_succ "Artifacts are ready in '$artifacts_path':"
+# Create zip archive:
+echo_subtitle "Creating '$XCF_ZIP_NAME' in '$artifacts_path'"
+cd "$artifacts_path" && zip -r -q $XCF_ZIP_NAME $XCF_DIR_NAME
+cd -
+
+# Cleanup:
+rm -rf "$artifacts_path/$XCF_DIR_NAME"
+
+echo_succ "Success. Artifacts ready in '$artifacts_path':"
 ls $artifacts_path
