4a5727008e960ba604e77dba1098b1e475c0ea7b
Jeff Ward <jeff.ward@datadoghq.com>
2025-02-26 10:02:30 -0500

    Switch `withNoOverflow` with `exactly` failable initializer

--- DatadogRUM/Sources/RUMMonitor/Scopes/RUMViewScope.swift
+++ DatadogRUM/Sources/RUMMonitor/Scopes/RUMViewScope.swift
@@ -534,15 +534,15 @@ internal class RUMViewScope: RUMScope, RUMContextProvider {
         var interactionToNextViewTime = interactionToNextViewMetric.value(for: viewUUID)
         // Only look for an internal override attribute if native INV calculations are disabled
         if interactionToNextViewTime == .failure(.disabled),
-           let customInvValue = internalAttributes[RUMInternalAttributes.customINVValue] as? (any BinaryInteger) {
-            let customInvValue = Int64(withNoOverflow: customInvValue)
+           let customInvValue = internalAttributes[RUMInternalAttributes.customINVValue] as? (any BinaryInteger),
+           let customInvValue = Int64(exactly: customInvValue) {
             interactionToNextViewTime = .success(TimeInterval(fromNanoseconds: customInvValue))
         }
 
         // Only add the performance member if we have a value for it
         let performance: RUMViewEvent.View.Performance?
-        if let fbcMetric = internalAttributes[RUMInternalAttributes.flutterFirstBuildComplete] as? (any BinaryInteger) {
-            let fbcMetric = Int64(withNoOverflow: fbcMetric)
+        if let fbcMetric = internalAttributes[RUMInternalAttributes.flutterFirstBuildComplete] as? (any BinaryInteger),
+           let fbcMetric = Int64(exactly: fbcMetric) {
             performance = .init(
                 cls: nil,
                 fbc: .init(timestamp: fbcMetric),
