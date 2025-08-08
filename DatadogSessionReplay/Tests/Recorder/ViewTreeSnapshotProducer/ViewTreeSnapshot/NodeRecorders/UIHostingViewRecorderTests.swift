b46685f71e37ecd3eccf42069369cff8a48fb00e
Guille Gonzalez <guille.gonzalez@datadoghq.com>
2025-08-08 16:24:23 +0200

    RUM-11242 Fix recorder tests

--- DatadogSessionReplay/Tests/Recorder/ViewTreeSnapshotProducer/ViewTreeSnapshot/NodeRecorders/UIHostingViewRecorderTests.swift
+++ DatadogSessionReplay/Tests/Recorder/ViewTreeSnapshotProducer/ViewTreeSnapshot/NodeRecorders/UIHostingViewRecorderTests.swift
@@ -94,12 +94,11 @@ class UIHostingViewRecorderTests: XCTestCase {
 
         let wireframes = try render(MockView())
         XCTAssertEqual(wireframes.count, 1)
-        let wireframe = try XCTUnwrap(wireframes.first?.shapeWireframe)
+        let wireframe = try XCTUnwrap(wireframes.first?.imageWireframe)
         XCTAssertEqual(wireframe.x, 100, accuracy: 5)
         XCTAssertEqual(wireframe.y, 130, accuracy: 5)
         XCTAssertEqual(wireframe.width, 100, accuracy: 5)
         XCTAssertEqual(wireframe.height, 100, accuracy: 5)
-        XCTAssertEqual(wireframe.shapeStyle?.backgroundColor, "#1022A00FF")
     }
 
     // MARK: Image
@@ -141,7 +140,7 @@ class UIHostingViewRecorderTests: XCTestCase {
         }
     }
 
-    func testImage_withUnsupportedType_itDoesNotRecordImage() throws {
+    func testImage_withShape_itDoesRecordSVGImage() throws {
         struct MockView: View {
             var body: some View {
                 Image(systemName: "star.fill")
@@ -160,7 +159,7 @@ class UIHostingViewRecorderTests: XCTestCase {
         )
 
         XCTAssertEqual(wireframes.count, 1)
-        let wireframe = try XCTUnwrap(wireframes.first?.shapeWireframe)
+        let wireframe = try XCTUnwrap(wireframes.first?.imageWireframe)
         XCTAssertEqual(wireframe.x, 125, accuracy: 5)
         XCTAssertEqual(wireframe.y, 155, accuracy: 5)
         XCTAssertEqual(wireframe.width, 50, accuracy: 5)
