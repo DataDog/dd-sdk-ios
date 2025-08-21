13a854520bd478c54f99508660367de7b1b36b45
Marie Denis <marie.denis@datadoghq.com>
2025-08-21 11:50:33 +0200

    RUM-11033 Update FO logic and add debug logs

--- DatadogRUM/Sources/RUMMonitor/Scopes/RUMSessionScope.swift
+++ DatadogRUM/Sources/RUMMonitor/Scopes/RUMSessionScope.swift
@@ -45,6 +45,10 @@ internal class RUMSessionScope: RUMScope, RUMContextProvider {
 
     /// Information about the application state since `RUM.enable()` was called.
     private let applicationState: RUMApplicationState
+    /// Feature Operation manager for processing Feature Operation commands.
+    private lazy var featureOperationManager: RUMFeatureOperationManager = {
+        RUMFeatureOperationManager(parent: self, dependencies: dependencies)
+    }()
 
     /// Information about this session state, shared with `CrashContext`.
     private var state: RUMSessionState {
@@ -239,6 +243,7 @@ internal class RUMSessionScope: RUMScope, RUMContextProvider {
             case let startViewCommand as RUMStartViewCommand:
                 // Start view scope explicitly on receiving "start view" command
                 startView(on: startViewCommand, context: context)
+
             case let appLifecycleCommand as RUMHandleAppLifecycleEventCommand where appLifecycleCommand.event == .didEnterBackground:
                 hadApplicationLaunchViewWhenEnteringBackground = activeViewPath == RUMOffViewEventsHandlingRule.Constants.applicationLaunchViewURL
 
@@ -249,8 +254,15 @@ internal class RUMSessionScope: RUMScope, RUMContextProvider {
                 hadApplicationLaunchViewWhenEnteringBackground = nil
 
             case let operationStepVitalCommand as RUMOperationStepVitalCommand:
-                sendFeatureOperationStepVitalEvent(on: operationStepVitalCommand, context: context, writer: writer)
-
+                let activeView = viewScopes.first { $0.isActiveView }
+                // Forward command to the feature operation manager
+                featureOperationManager.process(
+                    operationStepVitalCommand,
+                    context: context,
+                    writer: writer,
+                    activeViewID: (activeView?.viewUUID).orNull.toRUMDataFormat,
+                    activeViewPath: activeView?.viewPath ?? ""
+                )
             default:
                 if !hasActiveView {
                     handleOffViewCommand(command: command, context: context, writer: writer)
@@ -453,39 +465,5 @@ internal class RUMSessionScope: RUMScope, RUMContextProvider {
     private func hasExpired(currentTime: Date) -> Bool {
         let sessionDuration = currentTime.timeIntervalSince(sessionStartTime)
         return sessionDuration >= Constants.sessionMaxDuration
-    }
-
-    // MARK: - Feature Operation Step Vital Event Processing
-
-    private func sendFeatureOperationStepVitalEvent(on command: RUMOperationStepVitalCommand, context: DatadogContext, writer: Writer) {
-        let vital = RUMVitalEvent.Vital(
-            vitalDescription: nil,
-            duration: nil,
-            failureReason: command.failureReason,
-            id: command.vitalId,
-            name: command.name,
-            operationKey: command.operationKey,
-            stepType: command.stepType,
-            type: .operationStep
-        )
-
-        let vitalEvent = RUMVitalEvent(
-            dd: .init(),
-            application: .init(id: parent.context.rumApplicationID),
-            context: .init(contextInfo: command.globalAttributes.merging(command.attributes) { $1 }),
-            date: command.time.timeIntervalSince1970.toInt64Milliseconds,
-            session: .init(
-                hasReplay: context.hasReplay,
-                id: self.context.sessionID.toRUMDataFormat,
-                type: dependencies.sessionType
-            ),
-            view: .init(
-                id: parent.context.activeViewID.orNull.toRUMDataFormat,
-                url: parent.context.activeViewPath ?? ""
-            ),
-            vital: vital
-        )
-
-        writer.write(value: vitalEvent)
     }
 }
