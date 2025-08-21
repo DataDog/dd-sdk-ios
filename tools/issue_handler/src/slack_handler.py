cbd10d72032b03d8ab1b06c98dab97ff376ee136
Marie Denis <marie.denis@datadoghq.com>
2025-08-21 18:56:52 +0200

    Fix missing license headers

--- tools/issue_handler/src/slack_handler.py
+++ tools/issue_handler/src/slack_handler.py
@@ -1,3 +1,9 @@
+# -----------------------------------------------------------
+# Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+# This product includes software developed at Datadog, Inc.
+# Copyright 2019-Present Datadog, Inc.
+# -----------------------------------------------------------
+
 """
 Posts issue notifications and analysis to Slack using webhooks.
 """
