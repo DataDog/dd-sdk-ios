e4bd7d649c2353950c2277f3516c99e492de0af9
Simão Seiça <simao.seica@datadoghq.com>
2025-11-07 11:54:52 +0000

    RUM-12323: Skip profiling when TSan is enabled

--- DatadogProfiling/Mach/ctor_profiler.cpp
+++ DatadogProfiling/Mach/ctor_profiler.cpp
@@ -29,6 +29,24 @@ namespace dd::profiler { class ctor_profiler; }
 static dd::profiler::ctor_profiler* g_ctor_profiler = nullptr;
 
 /**
+ * Checks if ThreadSanitizer is enabled
+ * and without options to avoid halts.
+ *
+ * @return true if ThreadSanitizer is enabled, false otherwise
+ */
+bool is_thread_sanitizer_enabled() {
+#if __has_feature(thread_sanitizer)
+    const char* tsanOptions = getenv("TSAN_OPTIONS");
+    if (tsanOptions != nullptr) {
+        return (strstr(tsanOptions, "halt_on_error=0") == nullptr)
+        || (strstr(tsanOptions, "report_bugs=0") == nullptr);
+    }
+    return true;
+#endif
+    return false;
+}
+
+/**
  * Checks if the current process was launched via pre-warming by examining
  * the ActivePrewarm environment variable.
  *
@@ -137,6 +155,12 @@ public:
      * Start the profiler using constructor configuration
      */
     void start() {
+        if (is_thread_sanitizer_enabled()) {
+            printf("[DATADOG SDK] 🐶 → Profiling is disabled because ThreadSanitizer is active. Please disable ThreadSanitizer to enable profiling.\n");
+            status = CTOR_PROFILER_STATUS_NOT_STARTED;
+            return;
+        }
+
         if (is_prewarming) {
             status = CTOR_PROFILER_STATUS_PREWARMED;
             return;
