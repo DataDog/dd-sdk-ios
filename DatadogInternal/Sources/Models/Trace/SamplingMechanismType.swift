0d87ed7735fc0027a81fa9ede95f493dbe6107db
Miguel Arroz <miguel.arroz@datadoghq.com>
2026-01-14 15:21:30 +0000

    RUM-12403 Removed spurious punctuation in documentation.

--- DatadogInternal/Sources/Models/Trace/SamplingMechanismType.swift
+++ DatadogInternal/Sources/Models/Trace/SamplingMechanismType.swift
@@ -9,7 +9,7 @@ public enum SamplingMechanismType: Equatable {
     /// Fallback mechanism. This mechanism samples all spans. It should never be used, but it's included for completion.
     case fallback
     /// The main decision mechanism. Although the SDK runs in an agent-less scenario, we consider the SDK sampling
-    /// decisions act as the agent rate in scenarios with independent tracers and agent..
+    /// decisions act as the agent rate in scenarios with independent tracers and agent.
     case agentRate
     /// Decision mechanism used when a decision is manually set by the developer.
     case manual
