a0a81e28e5c79d95dccca02eeaea5b8766b2593d
Miguel Arroz <miguel.arroz@datadoghq.com>
2026-03-11 13:02:19 +0000

    RUM-14843: Removed spurious comma.

--- DatadogRUM/Tests/Instrumentation/Resources/URLSessionRUMResourcesHandlerTests.swift
+++ DatadogRUM/Tests/Instrumentation/Resources/URLSessionRUMResourcesHandlerTests.swift
@@ -84,7 +84,7 @@ class URLSessionRUMResourcesHandlerTests: XCTestCase {
                     applicationID: .mockRandom(),
                     sessionID: "abcdef01-2345-6789-abcd-ef0123456789"
                 ),
-                activeSpanProvider: activeSpanProvider,
+                activeSpanProvider: activeSpanProvider
             )
         )
 
