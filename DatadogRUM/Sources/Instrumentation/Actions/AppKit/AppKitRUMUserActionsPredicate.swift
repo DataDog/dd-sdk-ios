063d132604dc76adb63b7204687e48c6508baa84
Miguel Arroz <miguel.arroz@datadoghq.com>
2026-03-12 11:50:58 +0000

    Working on adapting instrumentation to NSControls. Partial work, not working yet.

---
+++ DatadogRUM/Sources/Instrumentation/Actions/AppKit/AppKitRUMUserActionsPredicate.swift
@@ -0,0 +1,67 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2019-Present Datadog, Inc.
+ */
+
+#if canImport(AppKit)
+import AppKit
+import DatadogInternal
+
+public typealias UIKitRUMActionsPredicate = AppKitRUMActionsPredicate
+
+/// The predicate for macOS interactions deciding if a given RUM Action should be recorded.
+///
+/// When the app is running, the SDK will ask the implementation of `UITouchRUMActionsPredicate` if any noticed user action on the target view should
+/// be considered as a RUM Action. The predicate implementation should return RUM Action parameters if it should be recorded or `nil` otherwise.
+public protocol AppKitRUMActionsPredicate {
+    /// The predicate deciding if the RUM Action should be recorded.
+    /// - Parameter targetView: an instance of the `UIView` which received the action.
+    /// - Returns: RUM Action if it should be recorded, `nil` otherwise.
+    func rumAction(targetView: DDView) -> RUMAction?
+}
+
+
+/// Default implementation of `UIKitRUMActionsPredicate`.
+/// It names  RUM Actions by the `accessibilityIdentifier` or `className` otherwise.
+public struct DefaultAppKitRUMActionsPredicate {
+    public init () {}
+
+    /// Builds the RUM Action's `target` name for given `DDView`.
+    private func targetName(for view: DDView) -> String {
+        let className = NSStringFromClass(type(of: view))
+
+//        if let accessibilityIdentifier = view.accessibilityIdentifier {
+//            return "\(className)(\(accessibilityIdentifier))"
+        // Some SwiftUI components are UIKit under the hood,
+        // but need to clean up tangled SwiftUI name
+        // e.g., _TtCV7SwiftUIP33_D74FE142C3C5A6C2CEA4987A69AEBD7522SystemSegmentedControl18UISegmentedControl
+//        } else
+        if view.isSwiftUIView {
+            return view.swiftUIViewName
+        } else {
+            return className
+        }
+    }
+}
+
+// MARK: iOS DefaultUIKitRUMActionsPredicate
+extension DefaultAppKitRUMActionsPredicate: AppKitRUMActionsPredicate {
+    public func rumAction(targetView: DDView) -> RUMAction? {
+        return RUMAction(
+            name: targetName(for: targetView),
+            attributes: [:]
+        )
+    }
+}
+
+private extension DDView {
+    var swiftUIViewName: String {
+        if typeDescription.hasPrefix("ViewBasedUIButton") {
+            return "SwiftUI_Menu"
+        }
+
+        return typeDescription
+    }
+}
+#endif
