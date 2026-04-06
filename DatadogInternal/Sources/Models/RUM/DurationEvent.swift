b2df31f77805d719d5d3fdda569d705d4d92f379
Simão Seiça <simao.seica@datadoghq.com>
2026-04-06 23:53:31 +0100

    RUM-14250: Update DataModels to have profiling schema

---
+++ DatadogInternal/Sources/Models/RUM/DurationEvent.swift
@@ -0,0 +1,31 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2019-Present Datadog, Inc.
+ */
+
+import Foundation
+
+public struct DurationEvent<T>: Codable, Equatable {
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case start = "start_ns"
+        case duration = "duration_ns"
+    }
+    /// UUID of the event
+    public let id: String
+    /// Start of the event from epoch in nanoseconds
+    public let start: Int64
+    /// Duration of the event in nanoseconds
+    public let duration: Int64
+
+    public init(
+        id: String,
+        start: Int64,
+        duration: Int64
+    ) {
+        self.id = id
+        self.start = start
+        self.duration = duration
+    }
+}
