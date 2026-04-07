6c34c4f23813f9dfc4d0f1153b317b89cee513a3
Simão Seiça <simao.seica@datadoghq.com>
2026-04-07 13:05:55 +0100

    RUM-14250: Create distinct messages for RUM events

---
+++ DatadogInternal/Sources/Models/RUM/LongTaskMessage.swift
@@ -0,0 +1,25 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2019-Present Datadog, Inc.
+ */
+
+import Foundation
+
+/// Message payload sent on the message bus when RUM reports a long task
+public struct LongTaskMessage {
+    /// Correlation context containing IDs for data correlation.
+    public let attributes: [String: AttributeValue]
+    /// Long task info for data correlation.
+    public let longTask: DurationEvent
+
+    /// Creates a new message payload.
+    ///
+    /// - Parameters:
+    ///   - attributes: Correlation context containing IDs for data correlation.
+    ///   - longTask: Long task info for data correlation.
+    public init(attributes: [String: AttributeValue], longTask: DurationEvent) {
+        self.attributes = attributes
+        self.longTask = longTask
+    }
+}
