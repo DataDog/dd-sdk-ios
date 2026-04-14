035fba6fd5f9afdfd49119eddfe67b389a38dfa6
Barbora Plasovska <barbora.plasovska@datadoghq.com>
2026-04-14 13:35:04 +0200

    add TimeseriesConfig for RUM context injection

---
+++ DatadogTimeseries/Sources/DatadogTimeseries/Core/TimeseriesConfig.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+struct TimeseriesConfig {
+    let applicationId: String
+    let sessionId: String
+    let sessionType: String
+    let source: String
+    let service: String?
+    let version: String?
+}
