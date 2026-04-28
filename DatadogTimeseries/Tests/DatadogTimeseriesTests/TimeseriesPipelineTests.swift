519859fdbaa28c6b50e2855d086f7a9f2fee4ef1
Barbora Plasovska <barbora.plasovska@datadoghq.com>
2026-04-28 16:39:37 +0200

    add license headers to DatadogTimeseries source and test files

--- DatadogTimeseries/Tests/DatadogTimeseriesTests/TimeseriesPipelineTests.swift
+++ DatadogTimeseries/Tests/DatadogTimeseriesTests/TimeseriesPipelineTests.swift
@@ -1,3 +1,8 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2019-Present Datadog, Inc.
+ */
 import XCTest
 @testable import DatadogTimeseries
 
@@ -60,7 +65,8 @@ final class TimeseriesPipelineTests: XCTestCase {
     }
 
     func testEmptyProviderProducesNoOutput() throws {
-        let csv = "timestamp,metric,value\n"
+        let csv = "timestamp,metric,value
+"
         let provider = CSVDataProvider(csvContent: csv, metric: .memoryUsage)
         let pipeline = TimeseriesPipeline(
             provider: provider,
