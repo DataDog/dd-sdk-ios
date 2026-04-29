070b1489714384f1cf981710871c19f5a0e50834
Barbora Plasovska <barbora.plasovska@datadoghq.com>
2026-04-29 13:59:07 +0200

    Fix wachos run

--- TestUtilities/Sources/Mocks/DatadogRUM/RUMFeatureMocks.swift
+++ TestUtilities/Sources/Mocks/DatadogRUM/RUMFeatureMocks.swift
@@ -1557,6 +1557,7 @@ public class RUMActionsHandlerMock: RUMActionsHandling {
         onViewModifierTapped?(actionName, actionAttributes)
     }
 }
+#endif
 
 public class SamplingBasedVitalReaderMock: SamplingBasedVitalReader {
     public var vitalData: Double?
@@ -1589,7 +1590,6 @@ public class ContinuousVitalReaderMock: ContinuousVitalReader {
         }
     }
 }
-#endif
 
 extension TelemetryReceiver: AnyMockable {
     public static func mockAny() -> Self { .mockWith() }
