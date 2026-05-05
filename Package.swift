d4c34f0340b3967600ec2f1e32c3b54482cf12dd
Miguel Arroz <miguel.arroz@datadoghq.com>
2026-05-05 14:27:09 +0100

    Random change to trigger CI

--- Package.swift
+++ Package.swift
@@ -264,7 +264,7 @@ let package = Package(
 )
 
 // If the `DD_TEST_UTILITIES_ENABLED` development ENV is set, export additional utility packages.
-// To set this ENV for Xcode projects that fetch this package locally, use `open --env DD_TEST_UTILITIES_ENABLED path/to/<project or workspace>`.
+// To set this ENV for Xcode projects that fetch this package locally, use `open --env  DD_TEST_UTILITIES_ENABLED path/to/<project or workspace>`.
 if ProcessInfo.processInfo.environment["DD_TEST_UTILITIES_ENABLED"] != nil {
     package.products.append(
         .library(
