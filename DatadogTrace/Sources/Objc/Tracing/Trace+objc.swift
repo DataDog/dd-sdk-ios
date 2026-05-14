44c8db7e3b489b129794f70040b694d353fb8b1c
Miguel Arroz <miguel.arroz@datadoghq.com>
2026-05-14 14:00:30 +0100

    Fixed lint errors.

--- DatadogTrace/Sources/Objc/Tracing/Trace+objc.swift
+++ DatadogTrace/Sources/Objc/Tracing/Trace+objc.swift
@@ -173,7 +173,7 @@ public final class objc_Tracer: NSObject, objc_OTTracer {
             swiftSpan: swiftTracer.startSpan(
                 operationName: operationName,
                 childOf: ddspanContext?.swiftSpanContext,
-                tags: tags.flatMap { castTagsToSwift($0) as? [String: OTTracer.TagValue]}
+                tags: tags.flatMap { castTagsToSwift($0) as? [String: OTTracer.TagValue] }
             )
         )
     }
@@ -190,7 +190,7 @@ public final class objc_Tracer: NSObject, objc_OTTracer {
             swiftSpan: swiftTracer.startSpan(
                 operationName: operationName,
                 childOf: ddspanContext?.swiftSpanContext,
-                tags: tags.flatMap { castTagsToSwift($0) as? [String: OTTracer.TagValue]},
+                tags: tags.flatMap { castTagsToSwift($0) as? [String: OTTracer.TagValue] },
                 startTime: startTime
             )
         )
