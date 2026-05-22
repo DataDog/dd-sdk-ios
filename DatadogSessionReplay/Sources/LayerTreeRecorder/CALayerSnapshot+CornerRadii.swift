0da54f029ae1f5e78292770d912a3f2cbf431485
Guille Gonzalez <guille.gonzalez@datadoghq.com>
2026-05-22 17:37:39 +0200

    PANA-7436 Add CALayerSnapshot capture for layer tree recording

---
+++ DatadogSessionReplay/Sources/LayerTreeRecorder/CALayerSnapshot+CornerRadii.swift
@@ -0,0 +1,42 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2019-Present Datadog, Inc.
+ */
+
+#if os(iOS)
+import Foundation
+import QuartzCore
+
+@available(iOS 13.0, tvOS 13.0, *)
+extension CALayerSnapshot {
+    struct CornerRadii: Sendable, Equatable {
+        var topLeft: CGSize
+        var topRight: CGSize
+        var bottomLeft: CGSize
+        var bottomRight: CGSize
+
+        init(
+            topLeft: CGSize = .zero,
+            topRight: CGSize = .zero,
+            bottomLeft: CGSize = .zero,
+            bottomRight: CGSize = .zero
+        ) {
+            self.topLeft = topLeft
+            self.topRight = topRight
+            self.bottomLeft = bottomLeft
+            self.bottomRight = bottomRight
+        }
+
+        init(cornerRadius: CGFloat, maskedCorners: CACornerMask) {
+            let cornerSize = CGSize(width: cornerRadius, height: cornerRadius)
+            self.init(
+                topLeft: maskedCorners.contains(.layerMinXMinYCorner) ? cornerSize : .zero,
+                topRight: maskedCorners.contains(.layerMaxXMinYCorner) ? cornerSize : .zero,
+                bottomLeft: maskedCorners.contains(.layerMinXMaxYCorner) ? cornerSize : .zero,
+                bottomRight: maskedCorners.contains(.layerMaxXMaxYCorner) ? cornerSize : .zero
+            )
+        }
+    }
+}
+#endif
