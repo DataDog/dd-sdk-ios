ae108b8023d672385864247b021afd0bd1b846fb
Barbora Plasovska <barbora.plasovska@datadoghq.com>
2026-07-09 17:27:28 +0200

    Update timeseries models to object-v2 schema, remove delta compression

--- DatadogRUM/Sources/RUMConfiguration.swift
+++ DatadogRUM/Sources/RUMConfiguration.swift
@@ -338,7 +338,7 @@ extension RUM {
 
         /// The number of samples collected before a timeseries batch is flushed.
         ///
-        /// Default: `30`.
+        /// Default: `120`.
         public var timeseriesBatchSize: Int
 
         /// Feature flags to preview features in RUM.
@@ -568,7 +568,7 @@ extension RUM.Configuration {
     ///   - telemetrySampleRate: The sampling rate for SDK internal telemetry utilized by Datadog. Must be a value between `0` and `100`. Default: `20`.
     ///   - collectAccessibility: Determines whether accessibility data should be collected and included in RUM view events. Default: `false`.
     ///   - enableTimeseries: Enables collection of memory and CPU timeseries events. Default: `false`.
-    ///   - timeseriesBatchSize: The number of samples collected before a timeseries batch is flushed. Default: `30`.
+    ///   - timeseriesBatchSize: The number of samples collected before a timeseries batch is flushed. Default: `120`.
     ///   - featureFlags: Experimental feature flags.
     /// 
     /// - Note: On watchOS, automatic UIKit and SwiftUI view/action tracking is unavailable. The predicate parameters will be ignored.
@@ -605,7 +605,7 @@ extension RUM.Configuration {
         telemetrySampleRate: SampleRate = 20,
         collectAccessibility: Bool = false,
         enableTimeseries: Bool = false,
-        timeseriesBatchSize: Int = 30,
+        timeseriesBatchSize: Int = 120,
         featureFlags: FeatureFlags = .defaults
     ) {
         self.applicationID = applicationID
@@ -664,7 +664,7 @@ extension RUM.Configuration {
         telemetrySampleRate: SampleRate = 20,
         collectAccessibility: Bool = false,
         enableTimeseries: Bool = false,
-        timeseriesBatchSize: Int = 30,
+        timeseriesBatchSize: Int = 120,
         featureFlags: FeatureFlags = .defaults
     ) {
         self.applicationID = applicationID
