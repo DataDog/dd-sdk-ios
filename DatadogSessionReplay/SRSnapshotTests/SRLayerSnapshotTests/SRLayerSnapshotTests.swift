888a48f7c8f66097b0de44a07b4c9ffb05ca6ac5
Guille Gonzalez <guille.gonzalez@datadoghq.com>
2026-07-15 08:28:08 +0200

    [PANA-8059] Update tab snapshots

--- DatadogSessionReplay/SRSnapshotTests/SRLayerSnapshotTests/SRLayerSnapshotTests.swift
+++ DatadogSessionReplay/SRSnapshotTests/SRLayerSnapshotTests/SRLayerSnapshotTests.swift
@@ -53,6 +53,7 @@ final class SRLayerSnapshotTests: LayerSnapshotTestCase {
     func testTab() async throws {
         try await takeLayerSnapshotFor(
             TabFixtureView(),
+            with: [.maskAll, .maskSensitiveInputs],
             shouldRecord: shouldRecord,
             folderPath: snapshotsFolderPath
         )
