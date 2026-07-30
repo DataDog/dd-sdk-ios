db12dcd429cdedfcadad429e0ef699fa74cd76a6
Maxime Epain <maxime.epain@datadoghq.com>
2026-07-30 11:18:42 +0200

    Bump KSCrash to 2.5.1

--- Package.swift
+++ Package.swift
@@ -54,7 +54,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/kstenerud/KSCrash.git", from: "2.5.0"),
+        .package(url: "https://github.com/kstenerud/KSCrash.git", from: "2.5.1"),
         .package(url: "https://github.com/open-telemetry/opentelemetry-swift-core", .upToNextMinor(from: "2.5.0")),
     ],
     targets: [
