f9d3c653aabb4a32a016c58edf8eaadd8d9db8fd
Miguel Arroz <miguel.arroz@datadoghq.com>
2026-08-28 16:51:21 +0100

    RUM-16718: Documented `MacOSSwiftUIComponentDetector`.

--- DatadogRUM/Sources/Instrumentation/Actions/SwiftUI/macOS/MacOSSwiftUIComponentDetector.swift
+++ DatadogRUM/Sources/Instrumentation/Actions/SwiftUI/macOS/MacOSSwiftUIComponentDetector.swift
@@ -13,11 +13,26 @@ import os.log
 internal struct MacOSSwiftUIComponentDetector: SwiftUIComponentDetector {
     typealias AccessibilityElement = AnyObject
 
+    /// Used to make sure `setupAxClient()` runs only once.
     @MainActor private static var axSetupPerformed = false
 
+    /// Creates an accessibility client for this application.
+    ///
+    /// The accessibility hierarchy will only be active if there is the application has a registered accessibility
+    /// client. Clients like VoiceOver, or external hardware drivers, register themselves as accessibility clients,
+    /// enabling the generation of accessibility hierarchies.
+    ///
+    /// Since SwiftUI instrumentation requires traversing the accessibility hierarchy, a client needs to be
+    /// registered. In this case, the application is an accessibility client of itself.
+    ///
+    /// This method can be called multiple times safely.
     @MainActor
     private static func setupAxClient() {
         if axSetupPerformed == false {
+            // Empirically, there is no need to keep `application` around after
+            // creating it and setting the role below. As long as a client registers,
+            // the accessibility machinery stays working even after the client
+            // is gone.
             let application = AXUIElementCreateApplication(
                 ProcessInfo.processInfo.processIdentifier
             )
@@ -37,6 +52,7 @@ internal struct MacOSSwiftUIComponentDetector: SwiftUIComponentDetector {
         }
     }
 
+    /// Accessibility roles corresponding to interactive elements worth instrumenting.
     private static let interestingAccessibilityRoles: Set<String> = [
         kAXButtonRole,
         kAXRadioButtonRole,
@@ -57,6 +73,22 @@ internal struct MacOSSwiftUIComponentDetector: SwiftUIComponentDetector {
         Self.setupAxClient()
     }
 
+    /// Creates a `RUMAddUserActionCommand` from an event based on the accessibility hierarchy.
+    ///
+    /// Since SwiftUI does not provide proper APIs to inspect the view hierarchy, the accessibility hierarchy is used to
+    /// instrument SwiftUI view hierarchies.
+    ///
+    /// Read the documentation of `bestActionTargetFor(accessibilityElement:coordinates:)` and
+    /// `traverseDownScrollViews(startingAt:coordinates:)` to understand more details of how the
+    /// hierarchy is traversed to find the most appropriate interactive element for the given event.
+    ///
+    /// - Parameters:
+    ///   - event: The event triggered by a user action.
+    ///   - predicate: Predicate indicating if SwiftUI actions should be recorded.
+    ///   - dateProvider: Provides dates for RUM events.
+    ///
+    /// - Returns: The command resulting from the given event, or `nil` if no action should be recorded for
+    /// this event.
     func createActionCommand(from event: NSEvent, predicate: (any SwiftUIRUMActionsPredicate)?, dateProvider: any DatadogInternal.DateProvider) -> RUMAddUserActionCommand? {
         guard
             let window = event.window,
@@ -66,7 +98,7 @@ internal struct MacOSSwiftUIComponentDetector: SwiftUIComponentDetector {
             return nil
         }
 
-        guard let targetElement = bestActionTargetFor(accessibilityElement: accessibilityElement, window: window, coordinates: coordsInScreen) else {
+        guard let targetElement = bestActionTargetFor(accessibilityElement: accessibilityElement, coordinates: coordsInScreen) else {
             return nil
         }
 
@@ -83,6 +115,15 @@ internal struct MacOSSwiftUIComponentDetector: SwiftUIComponentDetector {
         )
     }
 
+    /// Builds the name of the accessibility target, based on its role and identifier.
+    ///
+    /// If identifier is defined, the name will be something like "AXButton (Checkout)". Otherwise, the name will
+    /// only contain the role, as in "AXButton".
+    ///
+    /// - Parameters:
+    ///   - accessibilityElement: The element to obtain the name from.
+    ///
+    /// - Returns: The name of the target represented by `accessibilityElement`.
     private func targetName(for accessibilityElement: AccessibilityElement) -> String {
         let roleString = accessibilityElement.accessibilityRole().map { $0.rawValue } ?? "<unknown>"
         if let identifier = accessibilityElement.accessibilityIdentifier(), identifier.isEmpty == false {
@@ -92,8 +133,23 @@ internal struct MacOSSwiftUIComponentDetector: SwiftUIComponentDetector {
         }
     }
 
-    private func bestActionTargetFor(accessibilityElement: AccessibilityElement, window: NSWindow, coordinates: NSPoint) -> AccessibilityElement? {
-        var element: AccessibilityElement? = traverseDownScrollViews(startingAt: accessibilityElement, window: window, coordinates: coordinates)
+    /// Finds the interactive accessibility element in the given coordinates.
+    ///
+    /// Call this function with the result of `window.accessibilityHitTest` to further refine the result.
+    ///
+    /// If `accessibilityElement` is a scroll area, this method starts by traversing *down* the hierarchy looking for
+    /// the first element that is not a scroll area. This supports nested scroll areas. After obtaining such element (or using
+    /// `accessibilityElement` directly if it's not a scroll area), it traverses the hierarchy *up* looking for the first
+    /// interactive element.
+    ///
+    /// - Parameters:
+    ///   - accessibilityElement: The initial element returned by `window.accessibilityHitTest` for
+    ///   the given coordinates.
+    ///   - coordinates: The coordinates in the accessibility coordinate space.
+    ///
+    /// - Returns: The most appropriate interactive element as described above, or `nil` if no such element exists.
+    private func bestActionTargetFor(accessibilityElement: AccessibilityElement, coordinates: NSPoint) -> AccessibilityElement? {
+        var element: AccessibilityElement? = traverseDownScrollViews(startingAt: accessibilityElement, coordinates: coordinates)
 
         while let currentElement = element {
             if let role = axRole(currentElement),
@@ -107,8 +163,28 @@ internal struct MacOSSwiftUIComponentDetector: SwiftUIComponentDetector {
         return nil
     }
 
-    private func traverseDownScrollViews(startingAt accessibilityElement: AccessibilityElement, window: NSWindow, coordinates: NSPoint) -> AccessibilityElement {
+    /// If `accessibilityElement` is a scroll area (usually corresponding to a scroll view), it traverses the hierarchy
+    /// *down* until it finds the first element that is not a scroll area.
+    ///
+    /// This method supports nested scroll views. For example, if the user clicked a button inside a scroll view that is, itself,
+    /// inside another scroll view, this method traverses the hierarchy of both scroll views until it finds the button at the
+    /// given coordinates.
+    ///
+    /// - Remark: Empirically, an accessibility element corresponding to a scroll view (with a `AXScrollArea` role)
+    /// has, among its children, one of the type `AccessibilityLazyLayoutNode`. This node contains the actual
+    /// accessibility contents of the scroll view. This method assumes that hierarchy, and will traverse the hierarchy looking
+    /// for the `AccessibilityLazyLayoutNode` and performing a hit test on it to search for the next node. Since
+    /// this is not formally documented by Apple, this method may break in future versions of macOS.
+    ///
+    /// - parameters:
+    ///   - accessibilityElement: The accessibility element to start the search on.
+    ///   - coordinates: The event coordinates in accessibility coordinate space.
+    ///
+    /// - returns: If `accessibilityElement` is not a scroll area, returns the element itself. Otherwise, it traverses
+    /// the accessibility hierarchy down, as explained above, and returns the first element that is not a scroll area.
+    private func traverseDownScrollViews(startingAt accessibilityElement: AccessibilityElement, coordinates: NSPoint) -> AccessibilityElement {
         func isScrollView(_ element: AccessibilityElement) -> Bool {
+            // Should this be checking for AXScrollArea role?
             String(describing: type(of: element)).contains("HostingScrollView")
         }
 
@@ -134,6 +210,17 @@ internal struct MacOSSwiftUIComponentDetector: SwiftUIComponentDetector {
         return currentElement
     }
 
+    /// Obtains the accessibility role of a given accessibility element.
+    ///
+    /// - Note: Given how the informal accessibility protocols work, the only fully reliable way to safely call the
+    /// `accessibilityRole()` on a given object and avoid a crash is by making sure the object responds
+    /// to that selector. This method wraps that complexity.
+    ///
+    /// - Parameters:
+    ///   - element: The accessibility element the caller wants the role of.
+    ///
+    /// - returns: The accessibility role of the given element, or `nil` if the given element has no role, or
+    /// does not respond to `accessibilityRole()`.
     private func axRole(_ element: AccessibilityElement) -> NSAccessibility.Role? {
         guard element.responds(to: #selector(NSAccessibilityProtocol.accessibilityRole)),
               let role = element.accessibilityRole()
@@ -144,6 +231,17 @@ internal struct MacOSSwiftUIComponentDetector: SwiftUIComponentDetector {
         return role
     }
 
+    /// Obtains the accessibility parent of a given accessibility element.
+    ///
+    /// - Note: Given how the informal accessibility protocols work, the only fully reliable way to safely call the
+    /// `accessibilityParent()` on a given object and avoid a crash is by making sure the object responds
+    /// to that selector. This method wraps that complexity.
+    ///
+    /// - Parameters:
+    ///   - element: The accessibility element the caller wants the parent of.
+    ///
+    /// - returns: The accessibility parent of the given element, or `nil` if the given element has no parent, or
+    /// does not respond to `accessibilityParent()`.
     private func axParent(_ element: AccessibilityElement) -> AccessibilityElement? {
         guard element.responds(to: #selector(NSAccessibilityProtocol.accessibilityParent)),
               let parent = element.accessibilityParent() as? AccessibilityElement
@@ -154,6 +252,19 @@ internal struct MacOSSwiftUIComponentDetector: SwiftUIComponentDetector {
         return parent
     }
 
+    /// Obtains the result of calling `accessibilityHitTest` for the given coordinates on the given element.
+    ///
+    /// - Note: Given how the informal accessibility protocols work, the only fully reliable way to safely call the
+    /// `accessibilityHitTest(coords:)` on a given object and avoid a crash is by making sure the object
+    /// responds to that selector. This method wraps that complexity.
+    ///
+    /// - Parameters:
+    ///   - element: The accessibility element the caller wants to call `accessibilityHitTest` on.
+    ///   - coordinates: The coordinates to test, in the accessibility coordinate space.
+    ///
+    /// - returns: The result of calling `accessibilityHitTest` on the given element with the given
+    /// coordinates, or `nil` if `element` does not consider `coordinates` to be inside itself, or does not
+    /// respond to `accessibilityHitTest(coords:)`.
     private func axHitTesting(_ element: AccessibilityElement, coordinates: CGPoint) -> AccessibilityElement? {
         guard element.responds(to: #selector(NSObject.accessibilityHitTest)),
               let element = element.accessibilityHitTest(coordinates) as? AccessibilityElement
