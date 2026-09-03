4f98bbe6b68413342f51925fdd9a94aac6df2ea3
Miguel Arroz <miguel.arroz@datadoghq.com>
2026-09-03 12:12:14 +0100

    Fixed issue introduced during rebase.

--- DatadogRUM/Sources/Feature/RUMFeature.swift
+++ DatadogRUM/Sources/Feature/RUMFeature.swift
@@ -304,7 +304,8 @@ internal final class RUMFeature: DatadogRemoteFeature, RUMSessionSamplerProvider
             watchdogTermination: watchdogTermination,
             memoryWarningMonitor: nil,
             uuidGenerator: configuration.uuidGenerator,
-            heatmapIdentifierRegistry: heatmapIdentifierStore
+            heatmapIdentifierRegistry: heatmapIdentifierStore,
+            isAppHangBacktraceEnabled: isAppHangBacktraceEnabled
         )
         #elseif os(watchOS)
         self.instrumentation = RUMInstrumentation(
@@ -320,7 +321,8 @@ internal final class RUMFeature: DatadogRemoteFeature, RUMSessionSamplerProvider
             bundleType: bundleType,
             watchdogTermination: watchdogTermination,
             memoryWarningMonitor: nil,
-            uuidGenerator: configuration.uuidGenerator
+            uuidGenerator: configuration.uuidGenerator,
+            isAppHangBacktraceEnabled: isAppHangBacktraceEnabled
         )
         #else
         var memoryWarningMonitor: MemoryWarningMonitor?
