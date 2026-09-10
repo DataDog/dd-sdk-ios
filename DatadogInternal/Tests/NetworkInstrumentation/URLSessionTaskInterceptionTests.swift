86b4409db6b3d8bd5f23a9ad1098191950707460
Barbora Plašovská <barbora.plasovska@datadoghq.com>
2026-09-10 15:43:56 +0200

    RUM-18378 Replace local_cache_hit with delivery_type/transfer_size

--- DatadogInternal/Tests/NetworkInstrumentation/URLSessionTaskInterceptionTests.swift
+++ DatadogInternal/Tests/NetworkInstrumentation/URLSessionTaskInterceptionTests.swift
@@ -245,7 +245,8 @@ class ResourceMetricsTests: XCTestCase {
         XCTAssertEqual(resourceMetrics.responseBodySize?.decoded, taskTransaction.countOfResponseBodyBytesAfterDecoding)
         XCTAssertEqual(resourceMetrics.requestBodySize?.decoded, taskTransaction.countOfRequestBodyBytesBeforeEncoding)
         XCTAssertEqual(resourceMetrics.requestBodySize?.encoded, taskTransaction.countOfRequestBodyBytesSent)
-        XCTAssertEqual(resourceMetrics.isLocalCacheHit, false)
+        XCTAssertEqual(resourceMetrics.deliveryType, .other)
+        XCTAssertEqual(resourceMetrics.transferSize, taskTransaction.countOfResponseBodyBytesReceived)
     }
 
     func testWhenTaskMakesMultipleFetchesFromNetwork_thenAllMetricsAreCollected() {
@@ -361,16 +362,42 @@ class ResourceMetricsTests: XCTestCase {
             resourceMetrics.requestBodySize,
             "`requestBodySize` should not be tracked for cache transactions."
         )
-        XCTAssertEqual(resourceMetrics.isLocalCacheHit, true)
+        XCTAssertEqual(resourceMetrics.deliveryType, .cache)
+        XCTAssertEqual(resourceMetrics.transferSize, 0)
     }
 
-    func testWhenTaskFetchTypeVaries_thenLocalCacheHitReflectsOnlyKnownSignals() {
+    func testWhenTaskGets304RevalidationResponse_thenDeliveryTypeIsCacheWithSentinelTransferSize() {
+        let taskInterval = DateInterval(
+            start: .mockDecember15th2019At10AMUTC(),
+            end: .mockDecember15th2019At10AMUTC(addingTimeInterval: 5)
+        )
+        let response: URLResponse = .mockWith(statusCode: 304)
+        let taskTransaction: URLSessionTaskTransactionMetrics = .mockBySpreadingDetailsBetween(
+            start: taskInterval.start,
+            end: taskInterval.end,
+            resourceFetchType: .networkLoad,
+            response: response
+        )
+
+        // When
+        let taskMetrics: URLSessionTaskMetrics = .mockWith(
+            taskInterval: taskInterval,
+            transactionMetrics: [taskTransaction]
+        )
+
+        // Then
+        let resourceMetrics = ResourceMetrics(taskMetrics: taskMetrics)
+        XCTAssertEqual(resourceMetrics.deliveryType, .cache)
+        XCTAssertGreaterThan(resourceMetrics.transferSize ?? 0, 0)
+    }
+
+    func testWhenTaskFetchTypeVaries_thenDeliveryTypeReflectsOnlyKnownSignals() {
         // `.unknown` is documented by Apple as "the fetch manner was not determined" -
         // it must not be reported as a measured cache miss.
-        let cases: [(fetchType: URLSessionTaskMetrics.ResourceFetchType, expected: Bool?)] = [
-            (.localCache, true),
-            (.networkLoad, false),
-            (.serverPush, false),
+        let cases: [(fetchType: URLSessionTaskMetrics.ResourceFetchType, expected: ResourceMetrics.DeliveryType?)] = [
+            (.localCache, .cache),
+            (.networkLoad, .other),
+            (.serverPush, .other),
             (.unknown, nil)
         ]
 
@@ -391,7 +418,7 @@ class ResourceMetricsTests: XCTestCase {
                 )
 
                 let resourceMetrics = ResourceMetrics(taskMetrics: taskMetrics)
-                XCTAssertEqual(resourceMetrics.isLocalCacheHit, testCase.expected)
+                XCTAssertEqual(resourceMetrics.deliveryType, testCase.expected)
             }
         }
     }
