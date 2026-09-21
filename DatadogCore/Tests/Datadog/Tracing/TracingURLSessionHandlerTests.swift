b16fbd07e722d5b151aa938745b39f1aa9d10246
Valentin Pertuisot <valentin.pertuisot@datadoghq.com>
2026-09-21 05:59:42 +0200

    Align Trace header fixture with captured request session

--- DatadogCore/Tests/Datadog/Tracing/TracingURLSessionHandlerTests.swift
+++ DatadogCore/Tests/Datadog/Tracing/TracingURLSessionHandlerTests.swift
@@ -216,16 +216,17 @@ class TracingURLSessionHandlerTests: XCTestCase {
 
     func testGivenAllTracingHeaderTypes_itUsesTheSameIds() throws {
         let request: URLRequest = .mockWith(httpMethod: "GET")
-        let fakeSessionId: UUID = .mockWith("8b723a25-e941-47ea-9173-910c866ccf19")
-        let fakeContext: DatadogContext = .mockWith(
+        let receiverSessionID: UUID = .mockWith("8b723a25-e941-47ea-9173-910c866ccf19")
+        let requestSessionID: UUID = .mockWith("abcdef01-2345-6789-abcd-ef0123456789")
+        let receiverContext: DatadogContext = .mockWith(
             additionalContext: [
                 RUMCoreContext.mockWith(
                     applicationID: .mockRandom(),
-                    sessionID: fakeSessionId
+                    sessionID: receiverSessionID
                 )
             ]
         )
-        let message = FeatureMessage.context(fakeContext)
+        let message = FeatureMessage.context(receiverContext)
         _ = handler.contextReceiver.receive(message: message, from: core)
         let (modifiedRequest, _, _) = handler.modify(
             request: request,
@@ -233,7 +234,7 @@ class TracingURLSessionHandlerTests: XCTestCase {
             networkContext: NetworkContext(
                 rumContext: .mockWith(
                     applicationID: .mockRandom(),
-                    sessionID: .mockWith("abcdef01-2345-6789-abcd-ef0123456789")
+                    sessionID: requestSessionID
                 )
             )
         )
@@ -248,7 +249,7 @@ class TracingURLSessionHandlerTests: XCTestCase {
                 "b3": "000000000000000a0000000000000064-0000000000000064-1",
                 "x-datadog-trace-id": "100",
                 "x-datadog-tags": "_dd.p.tid=a,_dd.p.dm=-1",
-                "baggage": "session.id=\(fakeSessionId.uuidString.lowercased())",
+                "baggage": "session.id=\(requestSessionID.uuidString.lowercased())",
                 "tracestate": "dd=p:0000000000000064;s:1;t.dm:-1",
                 "x-datadog-parent-id": "100",
                 "x-datadog-sampling-priority": "1"
