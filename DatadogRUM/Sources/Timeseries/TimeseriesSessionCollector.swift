b4cd30bb736a1b05ad613aafddbe718601c2d952
Barbora Plašovská <barbora.plasovska@datadoghq.com>
2026-09-21 17:41:32 +0200

    Remove unused queue injection point

--- DatadogRUM/Sources/Timeseries/TimeseriesSessionCollector.swift
+++ DatadogRUM/Sources/Timeseries/TimeseriesSessionCollector.swift
@@ -82,7 +82,7 @@ internal class TimeseriesSessionCollector: TimeseriesCollecting {
     private var hasReplay: Bool? = nil
 
     /// All buffer mutations and timer events run on this queue.
-    private let queue: DispatchQueue
+    private let queue = DispatchQueue(label: "com.datadoghq.timeseries-collector", qos: .utility)
 
     init(
         memoryReader: SamplingBasedVitalReader,
@@ -96,8 +96,7 @@ internal class TimeseriesSessionCollector: TimeseriesCollecting {
         syntheticsTest: RUMSyntheticsTest? = nil,
         sessionSampleRate: Double = 100,
         now: @escaping () -> Date = Date.init,
-        mediaTimeProvider: CACurrentMediaTimeProvider = MediaTimeProvider(),
-        queue: DispatchQueue = DispatchQueue(label: "com.datadoghq.timeseries-collector", qos: .utility)
+        mediaTimeProvider: CACurrentMediaTimeProvider = MediaTimeProvider()
     ) {
         self.memoryReader = memoryReader
         self.batchSize = max(2, batchSize)
@@ -111,7 +110,6 @@ internal class TimeseriesSessionCollector: TimeseriesCollecting {
         self.cpuUsageProvider = cpuUsageProvider ?? { TimeseriesSessionCollector.processCPU() }
         self.now = now
         self.mediaTimeProvider = mediaTimeProvider
-        self.queue = queue
     }
 
     deinit {
