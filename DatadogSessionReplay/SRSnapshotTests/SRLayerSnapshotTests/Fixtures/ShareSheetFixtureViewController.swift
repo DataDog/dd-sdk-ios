bc10a1dff0711671055cfeeb4a440f3779cd34a6
Guille Gonzalez <guille.gonzalez@datadoghq.com>
2026-09-23 10:43:49 +0200

    [PANA-9100] Add share sheet coverage to layer snapshot tests

---
+++ DatadogSessionReplay/SRSnapshotTests/SRLayerSnapshotTests/Fixtures/ShareSheetFixtureViewController.swift
@@ -0,0 +1,24 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2019-Present Datadog, Inc.
+ */
+
+import UIKit
+
+internal final class ShareSheetFixtureViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+
+    func showShareSheet() async {
+        let activityViewController = UIActivityViewController(activityItems: [], applicationActivities: nil)
+
+        await withCheckedContinuation { continuation in
+            present(activityViewController, animated: false) {
+                continuation.resume()
+            }
+        }
+    }
+}
