f9b707520ee8dd1f866b44106539138ec667595d
Guille Gonzalez <guille.gonzalez@datadoghq.com>
2026-09-24 11:23:12 +0200

    [PANA-9100] Replace XCTest waiting with a checked continuation

--- DatadogSessionReplay/SRSnapshotTests/SRLayerSnapshotTests/Fixtures/SafariFixtureViewController.swift
+++ DatadogSessionReplay/SRSnapshotTests/SRLayerSnapshotTests/Fixtures/SafariFixtureViewController.swift
@@ -6,10 +6,9 @@
 
 import SafariServices
 import UIKit
-import XCTest
 
 internal final class SafariFixtureViewController: UIViewController, SFSafariViewControllerDelegate {
-    private let initialLoad = XCTestExpectation(description: "Safari initial load")
+    private var initialLoadContinuation: CheckedContinuation<Void, Never>?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,17 +19,15 @@ internal final class SafariFixtureViewController: UIViewController, SFSafariView
         let safariViewController = SFSafariViewController(url: URL(string: "http://127.0.0.1")!)
         safariViewController.delegate = self
 
-        let presentation = XCTestExpectation(description: "Safari presentation")
-        present(safariViewController, animated: false) {
-            presentation.fulfill()
+        await withCheckedContinuation { continuation in
+            initialLoadContinuation = continuation
+            present(safariViewController, animated: false)
         }
-
-        let result = await XCTWaiter.fulfillment(of: [presentation, initialLoad], timeout: 10)
-        XCTAssertEqual(result, .completed, "Safari did not finish presenting and loading within 10 seconds")
     }
 
     func safariViewController(_ controller: SFSafariViewController, didCompleteInitialLoad didLoadSuccessfully: Bool) {
         // A failed load is expected for the local URL and still means Safari is ready.
-        initialLoad.fulfill()
+        initialLoadContinuation?.resume()
+        initialLoadContinuation = nil
     }
 }
